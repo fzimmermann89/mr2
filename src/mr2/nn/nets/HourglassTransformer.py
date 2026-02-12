PTB-MR/mrpro

add hourglass transformer architecture and tests
ghstack-source-id: 17fee4c0bb1469e1edb7f3fe640ff4f79b576483
ghstack-comment-id: 3865651799
Pull-Request-resolved: https://github.com/PTB-MR/mrpro/pull/960

src/mrpro/nn/nets/HourglassTransformer.py
@@ -1,0 +1,147 @@
+"""Hourglass Transformer."""
+
+from collections.abc import Sequence
+from itertools import pairwise
+
+from torch.nn import Module
+
+from mrpro.nn.attention.SpatialTransformerBlock import SpatialTransformerBlock
+from mrpro.nn.join import Interpolate
+from mrpro.nn.nets.UNet import UNetBase, UNetDecoder, UNetEncoder
+from mrpro.nn.PixelShuffle import PixelShuffleUpsample, PixelUnshuffleDownsample
+from mrpro.nn.Sequential import Sequential
+from mrpro.operators.RearrangeOp import RearrangeOp
+from mrpro.utils.to_tuple import to_tuple
+
+
+class HourglassTransformer(UNetBase):
+    """Hourglass Transformer.
+
+    A U-shaped transformer [CK]_ with neighborhood self-attention [NAT]_.
+
+    References
+    ----------
+    .. [CK] Crowson, Katherine, et al. "Scalable high-resolution pixel-space image synthesis with
+        hourglass diffusion transformers." ICML 2024, https://arxiv.org/abs/2401.11605
+    .. [NAT] Hassani, A. et al. "Neighborhood Attention Transformer" CVPR, 2023, https://arxiv.org/abs/2204.07143
+
+    """
+
+    def __init__(
+        self,
+        n_dim: int,
+        n_channels_in: int,
+        n_channels_out: int,
+        n_features: Sequence[int] | int,
+        depths: Sequence[int] | int = 3,
+        attention_neighborhood: Sequence[None | int] | int | None = 11,
+        n_heads: int | Sequence[int] = 4,
+        cond_dim: int = 0,
+    ):
+        """Initialize the Hourglass Transformer.
+
+        Parameters
+        ----------
+        n_dim
+            Number of (spatial)dimensions of the input data.
+        n_channels_in
+            Number of channels in the input data.
+        n_channels_out
+            Number of channels in the output data.
+        n_features
+            Number of features in each stage.
+        depths
+            Number of layers in each stage.
+        attention_neighborhood
+            Neighborhood size for the neighborhood self-attention. If None, use global attention
+            for that stage.
+        n_heads
+            Number of heads in each stage.
+        cond_dim
+            Number of dimensions of the conditioning tensor.
+        """
+        n_layers_ = [
+            len(x)
+            for x in (n_features, depths, attention_neighborhood, n_heads)
+            if (x is not None and not isinstance(x, int))
+        ]
+        n_layers = n_layers_[0]
+
+        if any(x != n_layers_[0] for x in n_layers_):
+            raise ValueError('All arguments must have the same length or be scalars')
+
+        n_features_ = to_tuple(n_layers, n_features)
+        depths_ = to_tuple(n_layers, depths)
+        attention_neighborhood_ = to_tuple(n_layers, attention_neighborhood)
+        n_heads_ = to_tuple(n_layers, n_heads)
+
+        move_channels_last = RearrangeOp('batch  channels ...  -> batch ... channels')
+        first_block = Sequential(
+            move_channels_last,
+            PixelUnshuffleDownsample(n_dim, n_channels_in, n_features_[0], downscale_factor=2, features_last=True),
+        )
+        dim_group = (tuple(range(-n_dim - 1, -1)),)
+        encoder_blocks: list[Module] = []
+        decoder_blocks: list[Module] = []
+        merge_blocks: list[Module] = []
+        down_blocks: list[Module] = []
+        up_blocks: list[Module] = []
+        for channels, depth, neighborhood, head in zip(
+            n_features_[:-1],
+            depths_[:-1],
+            attention_neighborhood_[:-1],
+            n_heads_[:-1],
+            strict=True,
+        ):
+            encoder_blocks.append(
+                SpatialTransformerBlock(
+                    dim_groups=dim_group,
+                    channels=channels,
+                    depth=depth,
+                    attention_neighborhood=neighborhood,
+                    n_heads=head,
+                    rope_embed_fraction=1.0,
+                    cond_dim=cond_dim,
+                    features_last=True,
+                    norm='rms',
+                )
+            )
+            decoder_blocks.append(
+                SpatialTransformerBlock(
+                    dim_groups=dim_group,
+                    channels=channels,
+                    depth=depth,
+                    attention_neighborhood=neighborhood,
+                    n_heads=head,
+                    rope_embed_fraction=1.0,
+                    cond_dim=cond_dim,
+                    features_last=True,
+                    norm='rms',
+                )
+            )
+            merge_blocks.append(Interpolate())
+        for channels, channels_next in pairwise(n_features_):
+            down_blocks.append(
+                PixelUnshuffleDownsample(n_dim, channels, channels_next, downscale_factor=2, features_last=True)
+            )
+            up_blocks.append(PixelShuffleUpsample(n_dim, channels_next, channels, upscale_factor=2, features_last=True))
+
+        last_block = Sequential(
+            PixelShuffleUpsample(n_dim, n_features_[-1], n_channels_out, upscale_factor=2, features_last=True),
+            move_channels_last.H,  # moves channels back to front
+        )
+        middle_block = SpatialTransformerBlock(
+            dim_groups=dim_group,
+            channels=n_features_[-1],
+            depth=depths_[-1],
+            attention_neighborhood=attention_neighborhood_[-1],
+            n_heads=n_heads_[-1],
+            rope_embed_fraction=1.0,
+            cond_dim=cond_dim,
+            features_last=True,
+            norm='rms',
+        )
+        encoder = UNetEncoder(first_block, encoder_blocks, down_blocks, middle_block)
+        decoder = UNetDecoder(decoder_blocks, up_blocks, merge_blocks, last_block)
+
+        super().__init__(encoder, decoder)

src/mrpro/nn/nets/__init__.py
@@ -1,4 +1,5 @@
 from mrpro.nn.nets.BasicCNN import BasicCNN
+from mrpro.nn.nets.HourglassTransformer import HourglassTransformer
 from mrpro.nn.nets.Restormer import Restormer
 from mrpro.nn.nets.SwinIR import SwinIR
 from mrpro.nn.nets.UNet import AttentionGatedUNet, UNet
@@ -8,6 +9,7 @@ from mrpro.nn.nets.MLP import MLP
 __all__ = [
     "AttentionGatedUNet",
     "BasicCNN",
+    "HourglassTransformer",
     "MLP",
     "Restormer",
     "SwinIR",

tests/nn/nets/test_hourglass.py
@@ -1,0 +1,65 @@
+"""Test Hourglass Transformer"""
+
+from typing import cast
+
+import pytest
+import torch
+from mrpro.nn.nets import HourglassTransformer
+from tests.conftest import minimal_torch_26
+
+
+@minimal_torch_26
+@torch.no_grad()
+@pytest.mark.parametrize('torch_compile', [True, False], ids=['compiled', 'uncompiled'])
+@pytest.mark.parametrize(
+    'device',
+    [
+        pytest.param('cpu', id='cpu'),
+        pytest.param('cuda', marks=pytest.mark.cuda, id='cuda'),
+    ],
+)
+def test_hourglass_forward(torch_compile: bool, device: str) -> None:
+    """Test the forward pass of the hourglass."""
+    hourglass = HourglassTransformer(
+        n_dim=2,
+        n_channels_in=1,
+        n_channels_out=1,
+        n_features=64,
+        attention_neighborhood=(7, 7, None),
+        cond_dim=32,
+    )
+
+    x = torch.zeros(1, 1, 16, 16, device=device)
+    cond = torch.zeros(1, 32, device=device)
+    hourglass = hourglass.to(device)
+    x = x.to(device)
+    cond = cond.to(device)
+    if torch_compile:
+        hourglass = cast(HourglassTransformer, torch.compile(hourglass, dynamic=False))
+    y = hourglass(x, cond=cond)
+    assert y.shape == (1, 1, 16, 16)
+
+
+@minimal_torch_26
+@pytest.mark.cuda
+def test_hourglass_backward() -> None:
+    hourglass = HourglassTransformer(
+        n_dim=1,
+        n_channels_in=1,
+        n_channels_out=1,
+        n_features=64,
+        attention_neighborhood=(7, 7, None),
+        cond_dim=32,
+    ).cuda()
+
+    x = torch.zeros(1, 1, 16, requires_grad=True).cuda()
+    cond = torch.zeros(1, 32, requires_grad=True).cuda()
+    y = hourglass(x, cond=cond)
+    y.sum().backward()
+    assert x.grad is not None, 'x.grad is None'
+    assert not x.grad.isnan().any(), 'x.grad is NaN'
+    assert cond.grad is not None, 'cond.grad is None'
+    assert not cond.grad.isnan().any(), 'cond.grad is NaN'
+    for name, parameter in hourglass.named_parameters():
+        assert parameter.grad is not None, f'{name}.grad is None'
+        assert not parameter.grad.isnan().any(), f'{name}.grad is NaN'

fzimmermann89/mr2

src/mr2/nn/nets/HourglassTransformer.py
@@ -1,0 +1,147 @@
+"""Hourglass Transformer."""
+
+from collections.abc import Sequence
+from itertools import pairwise
+
+from torch.nn import Module
+
+from mr2.nn.attention.SpatialTransformerBlock import SpatialTransformerBlock
+from mr2.nn.join import Interpolate
+from mr2.nn.nets.UNet import UNetBase, UNetDecoder, UNetEncoder
+from mr2.nn.PixelShuffle import PixelShuffleUpsample, PixelUnshuffleDownsample
+from mr2.nn.Sequential import Sequential
+from mr2.operators.RearrangeOp import RearrangeOp
+from mr2.utils.to_tuple import to_tuple
+
+
+class HourglassTransformer(UNetBase):
+    """Hourglass Transformer.
+
+    A U-shaped transformer [CK]_ with neighborhood self-attention [NAT]_.
+
+    References
+    ----------
+    .. [CK] Crowson, Katherine, et al. "Scalable high-resolution pixel-space image synthesis with
+        hourglass diffusion transformers." ICML 2024, https://arxiv.org/abs/2401.11605
+    .. [NAT] Hassani, A. et al. "Neighborhood Attention Transformer" CVPR, 2023, https://arxiv.org/abs/2204.07143
+
+    """
+
+    def __init__(
+        self,
+        n_dim: int,
+        n_channels_in: int,
+        n_channels_out: int,
+        n_features: Sequence[int] | int,
+        depths: Sequence[int] | int = 3,
+        attention_neighborhood: Sequence[None | int] | int | None = 11,
+        n_heads: int | Sequence[int] = 4,
+        cond_dim: int = 0,
+    ):
+        """Initialize the Hourglass Transformer.
+
+        Parameters
+        ----------
+        n_dim
+            Number of (spatial)dimensions of the input data.
+        n_channels_in
+            Number of channels in the input data.
+        n_channels_out
+            Number of channels in the output data.
+        n_features
+            Number of features in each stage.
+        depths
+            Number of layers in each stage.
+        attention_neighborhood
+            Neighborhood size for the neighborhood self-attention. If None, use global attention
+            for that stage.
+        n_heads
+            Number of heads in each stage.
+        cond_dim
+            Number of dimensions of the conditioning tensor.
+        """
+        n_layers_ = [
+            len(x)
+            for x in (n_features, depths, attention_neighborhood, n_heads)
+            if (x is not None and not isinstance(x, int))
+        ]
+        n_layers = n_layers_[0]
+
+        if any(x != n_layers_[0] for x in n_layers_):
+            raise ValueError('All arguments must have the same length or be scalars')
+
+        n_features_ = to_tuple(n_layers, n_features)
+        depths_ = to_tuple(n_layers, depths)
+        attention_neighborhood_ = to_tuple(n_layers, attention_neighborhood)
+        n_heads_ = to_tuple(n_layers, n_heads)
+
+        move_channels_last = RearrangeOp('batch  channels ...  -> batch ... channels')
+        first_block = Sequential(
+            move_channels_last,
+            PixelUnshuffleDownsample(n_dim, n_channels_in, n_features_[0], downscale_factor=2, features_last=True),
+        )
+        dim_group = (tuple(range(-n_dim - 1, -1)),)
+        encoder_blocks: list[Module] = []
+        decoder_blocks: list[Module] = []
+        merge_blocks: list[Module] = []
+        down_blocks: list[Module] = []
+        up_blocks: list[Module] = []
+        for channels, depth, neighborhood, head in zip(
+            n_features_[:-1],
+            depths_[:-1],
+            attention_neighborhood_[:-1],
+            n_heads_[:-1],
+            strict=True,
+        ):
+            encoder_blocks.append(
+                SpatialTransformerBlock(
+                    dim_groups=dim_group,
+                    channels=channels,
+                    depth=depth,
+                    attention_neighborhood=neighborhood,
+                    n_heads=head,
+                    rope_embed_fraction=1.0,
+                    cond_dim=cond_dim,
+                    features_last=True,
+                    norm='rms',
+                )
+            )
+            decoder_blocks.append(
+                SpatialTransformerBlock(
+                    dim_groups=dim_group,
+                    channels=channels,
+                    depth=depth,
+                    attention_neighborhood=neighborhood,
+                    n_heads=head,
+                    rope_embed_fraction=1.0,
+                    cond_dim=cond_dim,
+                    features_last=True,
+                    norm='rms',
+                )
+            )
+            merge_blocks.append(Interpolate())
+        for channels, channels_next in pairwise(n_features_):
+            down_blocks.append(
+                PixelUnshuffleDownsample(n_dim, channels, channels_next, downscale_factor=2, features_last=True)
+            )
+            up_blocks.append(PixelShuffleUpsample(n_dim, channels_next, channels, upscale_factor=2, features_last=True))
+
+        last_block = Sequential(
+            PixelShuffleUpsample(n_dim, n_features_[-1], n_channels_out, upscale_factor=2, features_last=True),
+            move_channels_last.H,  # moves channels back to front
+        )
+        middle_block = SpatialTransformerBlock(
+            dim_groups=dim_group,
+            channels=n_features_[-1],
+            depth=depths_[-1],
+            attention_neighborhood=attention_neighborhood_[-1],
+            n_heads=n_heads_[-1],
+            rope_embed_fraction=1.0,
+            cond_dim=cond_dim,
+            features_last=True,
+            norm='rms',
+        )
+        encoder = UNetEncoder(first_block, encoder_blocks, down_blocks, middle_block)
+        decoder = UNetDecoder(decoder_blocks, up_blocks, merge_blocks, last_block)
+
+        super().__init__(encoder, decoder)

src/mr2/nn/nets/__init__.py
@@ -1,4 +1,5 @@
 from mr2.nn.nets.BasicCNN import BasicCNN
+from mr2.nn.nets.HourglassTransformer import HourglassTransformer
 from mr2.nn.nets.MLP import MLP
 from mr2.nn.nets.Restormer import Restormer
 from mr2.nn.nets.SwinIR import SwinIR
@@ -8,6 +9,7 @@ from mr2.nn.nets.UNet import AttentionGatedUNet, UNet
 __all__ = [
     "AttentionGatedUNet",
     "BasicCNN",
+    "HourglassTransformer",
     "MLP",
     "Restormer",
     "SwinIR",

tests/nn/nets/test_hourglass.py
@@ -1,0 +1,65 @@
+"""Test Hourglass Transformer"""
+
+from typing import cast
+
+import pytest
+import torch
+from mr2.nn.nets import HourglassTransformer
+from tests.conftest import minimal_torch_26
+
+
+@minimal_torch_26
+@torch.no_grad()
+@pytest.mark.parametrize('torch_compile', [True, False], ids=['compiled', 'uncompiled'])
+@pytest.mark.parametrize(
+    'device',
+    [
+        pytest.param('cpu', id='cpu'),
+        pytest.param('cuda', marks=pytest.mark.cuda, id='cuda'),
+    ],
+)
+def test_hourglass_forward(torch_compile: bool, device: str) -> None:
+    """Test the forward pass of the hourglass."""
+    hourglass = HourglassTransformer(
+        n_dim=2,
+        n_channels_in=1,
+        n_channels_out=1,
+        n_features=64,
+        attention_neighborhood=(7, 7, None),
+        cond_dim=32,
+    )
+
+    x = torch.zeros(1, 1, 16, 16, device=device)
+    cond = torch.zeros(1, 32, device=device)
+    hourglass = hourglass.to(device)
+    x = x.to(device)
+    cond = cond.to(device)
+    if torch_compile:
+        hourglass = cast(HourglassTransformer, torch.compile(hourglass, dynamic=False))
+    y = hourglass(x, cond=cond)
+    assert y.shape == (1, 1, 16, 16)
+
+
+@minimal_torch_26
+@pytest.mark.cuda
+def test_hourglass_backward() -> None:
+    hourglass = HourglassTransformer(
+        n_dim=1,
+        n_channels_in=1,
+        n_channels_out=1,
+        n_features=64,
+        attention_neighborhood=(7, 7, None),
+        cond_dim=32,
+    ).cuda()
+
+    x = torch.zeros(1, 1, 16, requires_grad=True).cuda()
+    cond = torch.zeros(1, 32, requires_grad=True).cuda()
+    y = hourglass(x, cond=cond)
+    y.sum().backward()
+    assert x.grad is not None, 'x.grad is None'
+    assert not x.grad.isnan().any(), 'x.grad is NaN'
+    assert cond.grad is not None, 'cond.grad is None'
+    assert not cond.grad.isnan().any(), 'cond.grad is NaN'
+    for name, parameter in hourglass.named_parameters():
+        assert parameter.grad is not None, f'{name}.grad is None'
+        assert not parameter.grad.isnan().any(), f'{name}.grad is NaN'