PTB-MR/mrpro

add unet, basic cnn, and residual blocks
ghstack-source-id: b9f3a6bbcec0cc87b83a45c7bc35b8771ed4749d
ghstack-comment-id: 3865651070
Pull-Request-resolved: https://github.com/PTB-MR/mrpro/pull/956

src/mrpro/nn/ResBlock.py
@@ -1,0 +1,71 @@
+"""Residual convolution block with two convolutions."""
+
+import torch
+from torch.nn import Identity, Module, SiLU
+
+from mrpro.nn.CondMixin import CondMixin
+from mrpro.nn.FiLM import FiLM
+from mrpro.nn.GroupNorm import GroupNorm
+from mrpro.nn.ndmodules import convND
+from mrpro.nn.Sequential import Sequential
+
+
+class ResBlock(CondMixin, Module):
+    """Residual convolution block with two convolutions."""
+
+    def __init__(self, n_dim: int, n_channels_in: int, n_channels_out: int, cond_dim: int) -> None:
+        """Initialize the ResBlock.
+
+        Parameters
+        ----------
+        n_dim
+            The number of dimensions, i.e. 1, 2 or 3.
+        n_channels_in
+            The number of channels in the input tensor.
+        n_channels_out
+            The number of channels in the output tensor.
+        cond_dim
+            The number of features in the conditioning tensor used in a FiLM.
+            If set to 0 no FiLM is used.
+
+        """
+        super().__init__()
+        self.rezero = torch.nn.Parameter(torch.tensor(0.1))
+        self.block = Sequential(
+            GroupNorm(n_channels_in),
+            SiLU(),
+            convND(n_dim)(n_channels_in, n_channels_out, kernel_size=3, padding=1),
+            GroupNorm(n_channels_out),
+            SiLU(),
+            convND(n_dim)(n_channels_out, n_channels_out, kernel_size=3, padding=1),
+        )
+        if cond_dim > 0:
+            self.block.insert(1, FiLM(n_channels_in, cond_dim))
+            self.block.insert(-2, FiLM(n_channels_out, cond_dim))
+
+        if n_channels_out == n_channels_in:
+            self.skip_connection: Module = Identity()
+        else:
+            self.skip_connection = convND(n_dim)(n_channels_in, n_channels_out, kernel_size=1)
+
+    def __call__(self, x: torch.Tensor, *, cond: torch.Tensor | None = None) -> torch.Tensor:
+        """Apply the ResBlock.
+
+        Parameters
+        ----------
+        x
+            The input tensor.
+        cond
+            A conditioning tensor to be used for FiLM.
+
+        Returns
+        -------
+            The output tensor.
+        """
+        return super().__call__(x, cond=cond)
+
+    def forward(self, x: torch.Tensor, *, cond: torch.Tensor | None = None) -> torch.Tensor:
+        """Apply the ResBlock."""
+        h = self.block(x, cond=cond)
+        x = self.skip_connection(x) + self.rezero * h
+        return x

src/mrpro/nn/SeparableResBlock.py
@@ -1,0 +1,89 @@
+"""Residual block with separable convolutions."""
+
+from collections.abc import Sequence
+
+import torch
+from torch.nn import Module, SiLU
+
+from mrpro.nn.FiLM import FiLM
+from mrpro.nn.GroupNorm import GroupNorm
+from mrpro.nn.ndmodules import convND
+from mrpro.nn.PermutedBlock import PermutedBlock
+from mrpro.nn.Sequential import Sequential
+
+
+class SeparableResBlock(Module):
+    """Residual block with separable convolutions."""
+
+    def __init__(
+        self,
+        dim_groups: Sequence[Sequence[int]],
+        n_channels_in: int,
+        n_channels_out: int,
+        cond_dim: int,
+    ) -> None:
+        """Initialize the SeparableResBlock.
+
+        Applies convolutions as separable convolutions with SilU activation and group normalization.
+        For example, if ``dim_groups = ((-1,-2), (-3))`` then one 2D convolution is applied to the last two dimensions,
+        and one 1D convolution is applied to the last dimension.
+        The order within the block is Norm->Activation->Conv.
+        The whole sequence for all dimension groups is performed twice, with optional FiLM conditioning in between.
+        So for two `dim_groups`, a total of 4 convolutions are applied.
+
+        Parameters
+        ----------
+        dim_groups
+            Sequence of dimension groups to use in the convolutions.
+        n_channels_in
+            Number of input channels.
+        n_channels_out
+            Number of output channels.
+        cond_dim
+            Number of channels in the conditioning tensor. If 0, no conditioning is applied.
+        """
+        super().__init__()
+        self.rezero = torch.nn.Parameter(torch.tensor(0.1))
+
+        def block(dims: Sequence[int], channels_in: int) -> Module:
+            return Sequential(
+                GroupNorm(channels_in),
+                SiLU(),
+                PermutedBlock(dims, convND(len(dims))(channels_in, n_channels_out, 3, padding=1)),
+            )
+
+        blocks = Sequential(*(block(d, n_channels_in if i == 0 else n_channels_out) for i, d in enumerate(dim_groups)))
+        if cond_dim > 0:
+            blocks.append(FiLM(n_channels_out, cond_dim))
+        blocks.extend(block(d, n_channels_out) for d in dim_groups)
+        self.block = blocks
+        self.skip_connection = None
+        if n_channels_in != n_channels_out:
+            self.skip_connection = torch.nn.Linear(n_channels_in, n_channels_out)
+
+    def __call__(self, x: torch.Tensor, *, cond: torch.Tensor | None = None) -> torch.Tensor:
+        """Apply the SeparableResBlock.
+
+        Parameters
+        ----------
+        x
+            Input tensor.
+        cond
+            Conditioning tensor.
+
+        Returns
+        -------
+            Output tensor with the same number and order of dimensions as the input.
+        """
+        return super().__call__(x, cond=cond)
+
+    def forward(self, x: torch.Tensor, *, cond: torch.Tensor | None = None) -> torch.Tensor:
+        """Apply the SeparableResBlock."""
+        h = self.block(x, cond=cond)
+        if self.skip_connection is None:
+            skip = x
+        else:
+            skip = torch.moveaxis(x, 1, -1)
+            skip = self.skip_connection(skip)
+            skip = torch.moveaxis(skip, -1, 1)
+        return skip + self.rezero * h

tests/nn/nets/test_cnn.py
@@ -1,0 +1,58 @@
+"""Tests for BasicCNN network."""
+
+from typing import cast
+
+import pytest
+import torch
+from mrpro.nn.nets import BasicCNN
+
+
+@pytest.mark.parametrize('torch_compile', [True, False], ids=['compiled', 'uncompiled'])
+@pytest.mark.parametrize(
+    'device',
+    [
+        pytest.param('cpu', id='cpu'),
+        pytest.param('cuda', marks=pytest.mark.cuda, id='cuda'),
+    ],
+)
+def test_cnn_forward(torch_compile: bool, device: str) -> None:
+    """Test the forward pass of the cnn."""
+    cnn = BasicCNN(
+        n_dim=2,
+        n_channels_in=1,
+        n_channels_out=1,
+        norm='layer',
+        n_features=(8, 8),
+    )
+
+    x = torch.zeros(1, 1, 16, 16, device=device)
+    cnn = cnn.to(device)
+    x = x.to(device)
+    if torch_compile:
+        cnn = cast(BasicCNN, torch.compile(cnn))
+    y = cnn(x)
+    assert y.shape == (1, 1, 16, 16)
+
+
+def test_cnn_backward() -> None:
+    cnn = BasicCNN(
+        n_dim=1,
+        n_channels_in=1,
+        n_channels_out=1,
+        norm='instance',
+        activation='silu',
+        n_features=(8, 8),
+        cond_dim=32,
+    )
+
+    x = torch.zeros(1, 1, 16, requires_grad=True)
+    cond = torch.zeros(1, 32, requires_grad=True)
+    y = cnn(x, cond=cond)
+    y.sum().backward()
+    assert x.grad is not None, 'x.grad is None'
+    assert not x.grad.isnan().any(), 'x.grad is NaN'
+    assert cond.grad is not None, 'cond.grad is None'
+    assert not cond.grad.isnan().any(), 'cond.grad is NaN'
+    for name, parameter in cnn.named_parameters():
+        assert parameter.grad is not None, f'{name}.grad is None'
+        assert not parameter.grad.isnan().any(), f'{name}.grad is NaN'

tests/nn/test_resblock.py
@@ -1,0 +1,56 @@
+"""Tests for ResBlock module."""
+
+from collections.abc import Sequence
+from typing import cast
+
+import pytest
+import torch
+from mrpro.nn import ResBlock
+from mrpro.utils import RandomGenerator
+
+
+@pytest.mark.parametrize('torch_compile', [True, False], ids=['compiled', 'eager'])
+@pytest.mark.parametrize(
+    'device',
+    [
+        pytest.param('cpu', id='cpu'),
+        pytest.param('cuda', id='cuda', marks=pytest.mark.cuda),
+    ],
+)
+@pytest.mark.parametrize(
+    ('dim', 'channels_in', 'channels_out', 'cond_dim', 'input_shape', 'cond_shape'),
+    [
+        (2, 32, 32, 16, (1, 32, 32, 32), (1, 16)),
+        (3, 64, 32, 0, (2, 64, 16, 16, 16), None),
+    ],
+)
+def test_resblock(
+    dim: int,
+    channels_in: int,
+    channels_out: int,
+    cond_dim: int,
+    input_shape: Sequence[int],
+    cond_shape: Sequence[int] | None,
+    device: str,
+    torch_compile: bool,
+) -> None:
+    """Test ResBlock output shape and backpropagation."""
+    rng = RandomGenerator(seed=42)
+    x = rng.float32_tensor(input_shape).to(device).requires_grad_(True)
+    cond = rng.float32_tensor(cond_shape).to(device).requires_grad_(True) if cond_shape else None
+    block = ResBlock(n_dim=dim, n_channels_in=channels_in, n_channels_out=channels_out, cond_dim=cond_dim).to(device)
+    if torch_compile:
+        block = cast(ResBlock, torch.compile(block, dynamic=False))
+    output = block(x, cond=cond)
+    assert output.shape == (input_shape[0], channels_out, *input_shape[2:]), (
+        f'Output shape {output.shape} != expected {(input_shape[0], channels_out, *input_shape[2:])}'
+    )
+    output.sum().backward()
+    assert x.grad is not None, 'No gradient computed for input'
+    assert not output.isnan().any(), 'NaN values in output'
+    assert not x.grad.isnan().any(), 'NaN values in input gradients'
+    assert block.block[-1].weight.grad is not None, 'No gradient computed for first Conv'
+    if cond is not None:
+        assert cond.grad is not None, 'No gradient computed for conditioning'
+        assert not cond.isnan().any(), 'NaN values in conditioning'
+        assert not cond.grad.isnan().any(), 'NaN values in conditioning gradients'

tests/nn/test_separableresblock.py
@@ -1,0 +1,58 @@
+"""Tests for SeparableResBlock module."""
+
+from collections.abc import Sequence
+from typing import cast
+
+import pytest
+import torch
+from mrpro.nn import SeparableResBlock
+from mrpro.utils import RandomGenerator
+
+
+@pytest.mark.parametrize('torch_compile', [True, False], ids=['compiled', 'eager'])
+@pytest.mark.parametrize(
+    'device',
+    [
+        pytest.param('cpu', id='cpu'),
+        pytest.param('cuda', id='cuda', marks=pytest.mark.cuda),
+    ],
+)
+@pytest.mark.parametrize(
+    ('dim_groups', 'channels_in', 'channels_out', 'cond_dim', 'input_shape', 'cond_shape'),
+    [
+        (((-1, -2),), 32, 32, 16, (1, 32, 32, 32), (1, 16)),
+        (((-1, -2), (-3,)), 64, 32, 0, (2, 64, 16, 16, 16), None),  # 2D + 1D
+    ],
+)
+def test_separable_resblock(
+    dim_groups: Sequence[Sequence[int]],
+    channels_in: int,
+    channels_out: int,
+    cond_dim: int,
+    input_shape: Sequence[int],
+    cond_shape: Sequence[int] | None,
+    device: str,
+    torch_compile: bool,
+) -> None:
+    """Test SeparableResBlock output shape and backpropagation."""
+    rng = RandomGenerator(seed=42)
+    x = rng.float32_tensor(input_shape).to(device).requires_grad_(True)
+    cond = rng.float32_tensor(cond_shape).to(device).requires_grad_(True) if cond_shape else None
+    block = SeparableResBlock(
+        dim_groups=dim_groups, n_channels_in=channels_in, n_channels_out=channels_out, cond_dim=cond_dim
+    ).to(device)
+    if torch_compile:
+        block = cast(SeparableResBlock, torch.compile(block, dynamic=False))
+    output = block(x, cond=cond)
+    assert output.shape == (input_shape[0], channels_out, *input_shape[2:]), (
+        f'Output shape {output.shape} != expected {(input_shape[0], channels_out, *input_shape[2:])}'
+    )
+    output.sum().backward()
+    assert x.grad is not None, 'No gradient computed for input'
+    assert not output.isnan().any(), 'NaN values in output'
+    assert not x.grad.isnan().any(), 'NaN values in input gradients'
+    assert block.block[0][2].module.weight.grad is not None, 'No gradient computed for first Conv'  # type: ignore[union-attr]
+    if cond is not None:
+        assert cond.grad is not None, 'No gradient computed for conditioning'
+        assert not cond.isnan().any(), 'NaN values in conditioning'
+        assert not cond.grad.isnan().any(), 'NaN values in conditioning gradients'

fzimmermann89/mr2

src/mr2/nn/ResBlock.py
@@ -1,0 +1,71 @@
+"""Residual convolution block with two convolutions."""
+
+import torch
+from torch.nn import Identity, Module, SiLU
+
+from mr2.nn.CondMixin import CondMixin
+from mr2.nn.FiLM import FiLM
+from mr2.nn.GroupNorm import GroupNorm
+from mr2.nn.ndmodules import convND
+from mr2.nn.Sequential import Sequential
+
+
+class ResBlock(CondMixin, Module):
+    """Residual convolution block with two convolutions."""
+
+    def __init__(self, n_dim: int, n_channels_in: int, n_channels_out: int, cond_dim: int) -> None:
+        """Initialize the ResBlock.
+
+        Parameters
+        ----------
+        n_dim
+            The number of dimensions, i.e. 1, 2 or 3.
+        n_channels_in
+            The number of channels in the input tensor.
+        n_channels_out
+            The number of channels in the output tensor.
+        cond_dim
+            The number of features in the conditioning tensor used in a FiLM.
+            If set to 0 no FiLM is used.
+
+        """
+        super().__init__()
+        self.rezero = torch.nn.Parameter(torch.tensor(0.1))
+        self.block = Sequential(
+            GroupNorm(n_channels_in),
+            SiLU(),
+            convND(n_dim)(n_channels_in, n_channels_out, kernel_size=3, padding=1),
+            GroupNorm(n_channels_out),
+            SiLU(),
+            convND(n_dim)(n_channels_out, n_channels_out, kernel_size=3, padding=1),
+        )
+        if cond_dim > 0:
+            self.block.insert(1, FiLM(n_channels_in, cond_dim))
+            self.block.insert(-2, FiLM(n_channels_out, cond_dim))
+
+        if n_channels_out == n_channels_in:
+            self.skip_connection: Module = Identity()
+        else:
+            self.skip_connection = convND(n_dim)(n_channels_in, n_channels_out, kernel_size=1)
+
+    def __call__(self, x: torch.Tensor, *, cond: torch.Tensor | None = None) -> torch.Tensor:
+        """Apply the ResBlock.
+
+        Parameters
+        ----------
+        x
+            The input tensor.
+        cond
+            A conditioning tensor to be used for FiLM.
+
+        Returns
+        -------
+            The output tensor.
+        """
+        return super().__call__(x, cond=cond)
+
+    def forward(self, x: torch.Tensor, *, cond: torch.Tensor | None = None) -> torch.Tensor:
+        """Apply the ResBlock."""
+        h = self.block(x, cond=cond)
+        x = self.skip_connection(x) + self.rezero * h
+        return x

src/mr2/nn/SeparableResBlock.py
@@ -1,0 +1,89 @@
+"""Residual block with separable convolutions."""
+
+from collections.abc import Sequence
+
+import torch
+from torch.nn import Module, SiLU
+
+from mr2.nn.FiLM import FiLM
+from mr2.nn.GroupNorm import GroupNorm
+from mr2.nn.ndmodules import convND
+from mr2.nn.PermutedBlock import PermutedBlock
+from mr2.nn.Sequential import Sequential
+
+
+class SeparableResBlock(Module):
+    """Residual block with separable convolutions."""
+
+    def __init__(
+        self,
+        dim_groups: Sequence[Sequence[int]],
+        n_channels_in: int,
+        n_channels_out: int,
+        cond_dim: int,
+    ) -> None:
+        """Initialize the SeparableResBlock.
+
+        Applies convolutions as separable convolutions with SilU activation and group normalization.
+        For example, if ``dim_groups = ((-1,-2), (-3))`` then one 2D convolution is applied to the last two dimensions,
+        and one 1D convolution is applied to the last dimension.
+        The order within the block is Norm->Activation->Conv.
+        The whole sequence for all dimension groups is performed twice, with optional FiLM conditioning in between.
+        So for two `dim_groups`, a total of 4 convolutions are applied.
+
+        Parameters
+        ----------
+        dim_groups
+            Sequence of dimension groups to use in the convolutions.
+        n_channels_in
+            Number of input channels.
+        n_channels_out
+            Number of output channels.
+        cond_dim
+            Number of channels in the conditioning tensor. If 0, no conditioning is applied.
+        """
+        super().__init__()
+        self.rezero = torch.nn.Parameter(torch.tensor(0.1))
+
+        def block(dims: Sequence[int], channels_in: int) -> Module:
+            return Sequential(
+                GroupNorm(channels_in),
+                SiLU(),
+                PermutedBlock(dims, convND(len(dims))(channels_in, n_channels_out, 3, padding=1)),
+            )
+
+        blocks = Sequential(*(block(d, n_channels_in if i == 0 else n_channels_out) for i, d in enumerate(dim_groups)))
+        if cond_dim > 0:
+            blocks.append(FiLM(n_channels_out, cond_dim))
+        blocks.extend(block(d, n_channels_out) for d in dim_groups)
+        self.block = blocks
+        self.skip_connection = None
+        if n_channels_in != n_channels_out:
+            self.skip_connection = torch.nn.Linear(n_channels_in, n_channels_out)
+
+    def __call__(self, x: torch.Tensor, *, cond: torch.Tensor | None = None) -> torch.Tensor:
+        """Apply the SeparableResBlock.
+
+        Parameters
+        ----------
+        x
+            Input tensor.
+        cond
+            Conditioning tensor.
+
+        Returns
+        -------
+            Output tensor with the same number and order of dimensions as the input.
+        """
+        return super().__call__(x, cond=cond)
+
+    def forward(self, x: torch.Tensor, *, cond: torch.Tensor | None = None) -> torch.Tensor:
+        """Apply the SeparableResBlock."""
+        h = self.block(x, cond=cond)
+        if self.skip_connection is None:
+            skip = x
+        else:
+            skip = torch.moveaxis(x, 1, -1)
+            skip = self.skip_connection(skip)
+            skip = torch.moveaxis(skip, -1, 1)
+        return skip + self.rezero * h

tests/nn/nets/test_cnn.py
@@ -1,0 +1,58 @@
+"""Tests for BasicCNN network."""
+
+from typing import cast
+
+import pytest
+import torch
+from mr2.nn.nets import BasicCNN
+
+
+@pytest.mark.parametrize('torch_compile', [True, False], ids=['compiled', 'uncompiled'])
+@pytest.mark.parametrize(
+    'device',
+    [
+        pytest.param('cpu', id='cpu'),
+        pytest.param('cuda', marks=pytest.mark.cuda, id='cuda'),
+    ],
+)
+def test_cnn_forward(torch_compile: bool, device: str) -> None:
+    """Test the forward pass of the cnn."""
+    cnn = BasicCNN(
+        n_dim=2,
+        n_channels_in=1,
+        n_channels_out=1,
+        norm='layer',
+        n_features=(8, 8),
+    )
+
+    x = torch.zeros(1, 1, 16, 16, device=device)
+    cnn = cnn.to(device)
+    x = x.to(device)
+    if torch_compile:
+        cnn = cast(BasicCNN, torch.compile(cnn))
+    y = cnn(x)
+    assert y.shape == (1, 1, 16, 16)
+
+
+def test_cnn_backward() -> None:
+    cnn = BasicCNN(
+        n_dim=1,
+        n_channels_in=1,
+        n_channels_out=1,
+        norm='instance',
+        activation='silu',
+        n_features=(8, 8),
+        cond_dim=32,
+    )
+
+    x = torch.zeros(1, 1, 16, requires_grad=True)
+    cond = torch.zeros(1, 32, requires_grad=True)
+    y = cnn(x, cond=cond)
+    y.sum().backward()
+    assert x.grad is not None, 'x.grad is None'
+    assert not x.grad.isnan().any(), 'x.grad is NaN'
+    assert cond.grad is not None, 'cond.grad is None'
+    assert not cond.grad.isnan().any(), 'cond.grad is NaN'
+    for name, parameter in cnn.named_parameters():
+        assert parameter.grad is not None, f'{name}.grad is None'
+        assert not parameter.grad.isnan().any(), f'{name}.grad is NaN'

tests/nn/test_resblock.py
@@ -1,0 +1,56 @@
+"""Tests for ResBlock module."""
+
+from collections.abc import Sequence
+from typing import cast
+
+import pytest
+import torch
+from mr2.nn import ResBlock
+from mr2.utils import RandomGenerator
+
+
+@pytest.mark.parametrize('torch_compile', [True, False], ids=['compiled', 'eager'])
+@pytest.mark.parametrize(
+    'device',
+    [
+        pytest.param('cpu', id='cpu'),
+        pytest.param('cuda', id='cuda', marks=pytest.mark.cuda),
+    ],
+)
+@pytest.mark.parametrize(
+    ('dim', 'channels_in', 'channels_out', 'cond_dim', 'input_shape', 'cond_shape'),
+    [
+        (2, 32, 32, 16, (1, 32, 32, 32), (1, 16)),
+        (3, 64, 32, 0, (2, 64, 16, 16, 16), None),
+    ],
+)
+def test_resblock(
+    dim: int,
+    channels_in: int,
+    channels_out: int,
+    cond_dim: int,
+    input_shape: Sequence[int],
+    cond_shape: Sequence[int] | None,
+    device: str,
+    torch_compile: bool,
+) -> None:
+    """Test ResBlock output shape and backpropagation."""
+    rng = RandomGenerator(seed=42)
+    x = rng.float32_tensor(input_shape).to(device).requires_grad_(True)
+    cond = rng.float32_tensor(cond_shape).to(device).requires_grad_(True) if cond_shape else None
+    block = ResBlock(n_dim=dim, n_channels_in=channels_in, n_channels_out=channels_out, cond_dim=cond_dim).to(device)
+    if torch_compile:
+        block = cast(ResBlock, torch.compile(block, dynamic=False))
+    output = block(x, cond=cond)
+    assert output.shape == (input_shape[0], channels_out, *input_shape[2:]), (
+        f'Output shape {output.shape} != expected {(input_shape[0], channels_out, *input_shape[2:])}'
+    )
+    output.sum().backward()
+    assert x.grad is not None, 'No gradient computed for input'
+    assert not output.isnan().any(), 'NaN values in output'
+    assert not x.grad.isnan().any(), 'NaN values in input gradients'
+    assert block.block[-1].weight.grad is not None, 'No gradient computed for first Conv'
+    if cond is not None:
+        assert cond.grad is not None, 'No gradient computed for conditioning'
+        assert not cond.isnan().any(), 'NaN values in conditioning'
+        assert not cond.grad.isnan().any(), 'NaN values in conditioning gradients'

tests/nn/test_separableresblock.py
@@ -1,0 +1,58 @@
+"""Tests for SeparableResBlock module."""
+
+from collections.abc import Sequence
+from typing import cast
+
+import pytest
+import torch
+from mr2.nn import SeparableResBlock
+from mr2.utils import RandomGenerator
+
+
+@pytest.mark.parametrize('torch_compile', [True, False], ids=['compiled', 'eager'])
+@pytest.mark.parametrize(
+    'device',
+    [
+        pytest.param('cpu', id='cpu'),
+        pytest.param('cuda', id='cuda', marks=pytest.mark.cuda),
+    ],
+)
+@pytest.mark.parametrize(
+    ('dim_groups', 'channels_in', 'channels_out', 'cond_dim', 'input_shape', 'cond_shape'),
+    [
+        (((-1, -2),), 32, 32, 16, (1, 32, 32, 32), (1, 16)),
+        (((-1, -2), (-3,)), 64, 32, 0, (2, 64, 16, 16, 16), None),  # 2D + 1D
+    ],
+)
+def test_separable_resblock(
+    dim_groups: Sequence[Sequence[int]],
+    channels_in: int,
+    channels_out: int,
+    cond_dim: int,
+    input_shape: Sequence[int],
+    cond_shape: Sequence[int] | None,
+    device: str,
+    torch_compile: bool,
+) -> None:
+    """Test SeparableResBlock output shape and backpropagation."""
+    rng = RandomGenerator(seed=42)
+    x = rng.float32_tensor(input_shape).to(device).requires_grad_(True)
+    cond = rng.float32_tensor(cond_shape).to(device).requires_grad_(True) if cond_shape else None
+    block = SeparableResBlock(
+        dim_groups=dim_groups, n_channels_in=channels_in, n_channels_out=channels_out, cond_dim=cond_dim
+    ).to(device)
+    if torch_compile:
+        block = cast(SeparableResBlock, torch.compile(block, dynamic=False))
+    output = block(x, cond=cond)
+    assert output.shape == (input_shape[0], channels_out, *input_shape[2:]), (
+        f'Output shape {output.shape} != expected {(input_shape[0], channels_out, *input_shape[2:])}'
+    )
+    output.sum().backward()
+    assert x.grad is not None, 'No gradient computed for input'
+    assert not output.isnan().any(), 'NaN values in output'
+    assert not x.grad.isnan().any(), 'NaN values in input gradients'
+    assert block.block[0][2].module.weight.grad is not None, 'No gradient computed for first Conv'  # type: ignore[union-attr]
+    if cond is not None:
+        assert cond.grad is not None, 'No gradient computed for conditioning'
+        assert not cond.isnan().any(), 'NaN values in conditioning'
+        assert not cond.grad.isnan().any(), 'NaN values in conditioning gradients'