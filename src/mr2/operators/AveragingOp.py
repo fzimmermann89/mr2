PTB-MR/mrpro

refactor: singel normalize axes function

src/mrpro/operators/AveragingOp.py
@@ -6,6 +6,7 @@ from warnings import warn
 import torch
 
 from mrpro.operators.LinearOperator import LinearOperator
+from mrpro.utils.reshape import normalize_index
 
 
 class AveragingOp(LinearOperator):
@@ -104,8 +105,9 @@ class AveragingOp(LinearOperator):
             )
             self.domain_size = self._last_domain_size
 
-        adjoint = x.new_zeros(*x.shape[: self.dim], self.domain_size, *x.shape[self.dim + 1 :])
-        placeholder = (slice(None),) * (self.dim % x.ndim)
+        dim = normalize_index(x.ndim, self.dim)
+        adjoint = x.new_zeros(*x.shape[:dim], self.domain_size, *x.shape[dim + 1 :])
+        placeholder = (slice(None),) * dim
         for i, group in enumerate(self.idx):
             if isinstance(group, slice):
                 n = len(range(*group.indices(self.domain_size)))
@@ -115,7 +117,7 @@ class AveragingOp(LinearOperator):
                 n = len(group)
 
             adjoint[(*placeholder, group)] += (
-                x[(*placeholder, i, None)].expand(*x.shape[: self.dim], n, *x.shape[self.dim + 1 :]) / n
+                x[(*placeholder, i, None)].expand(*x.shape[:dim], n, *x.shape[dim + 1 :]) / n
             )
 
         return (adjoint,)

src/mrpro/utils/fill_range.py
@@ -2,6 +2,8 @@
 
 import torch
 
+from mrpro.utils.reshape import normalize_index
+
 
 def fill_range_(tensor: torch.Tensor, dim: int) -> None:
     """
@@ -15,10 +17,7 @@ def fill_range_(tensor: torch.Tensor, dim: int) -> None:
     dim
         The dimension along which to fill with increasing values.
     """
-    if not -tensor.ndim <= dim < tensor.ndim:
-        raise IndexError(f'Dimension {dim} is out of range for tensor with {tensor.ndim} dimensions.')
-
-    dim = dim % tensor.ndim
+    dim = normalize_index(tensor.ndim, dim)
     shape = [s if d == dim else 1 for d, s in enumerate(tensor.shape)]
     values = torch.arange(tensor.size(dim), device=tensor.device).reshape(shape)
     tensor[:] = values.expand_as(tensor)

src/mrpro/utils/interpolate.py
@@ -5,6 +5,8 @@ from typing import Literal
 
 import torch
 
+from mrpro.utils.reshape import normalize_indices
+
 
 def interpolate(
     x: torch.Tensor, size: Sequence[int], dim: Sequence[int], mode: Literal['nearest', 'linear'] = 'linear'
@@ -30,10 +32,7 @@ def interpolate(
     if len(dim) != len(size):
         raise ValueError('Must provide matching length size and dim arguments.')
 
-    # normalize dim to allow negative indexing in input
-    dim = tuple([a % x.ndim for a in dim])
-    if len(dim) != len(set(dim)):
-        raise ValueError(f'Dim must be unique. Normalized dims are {dim}')
+    dim = normalize_indices(x.ndim, dim)
 
     # return input tensor if old and new size match
     if all(x.shape[d] == s for s, d in zip(size, dim, strict=True)):

src/mrpro/utils/pad_or_crop.py
@@ -6,8 +6,7 @@ from typing import Literal
 
 import torch
 
-from mrpro.utils.indexing import normalize_index
-from mrpro.utils.reshape import unsqueeze_left
+from mrpro.utils.reshape import normalize_index, unsqueeze_left
 
 
 def pad_or_crop(

tests/operators/functionals/test_functionals.py
@@ -6,6 +6,7 @@ import torch
 from mrpro.operators.Functional import ElementaryFunctional, ElementaryProximableFunctional
 from mrpro.operators.functionals import MSE, L1Norm, L1NormViewAsReal, L2NormSquared, ZeroFunctional
 from mrpro.utils import RandomGenerator
+from mrpro.utils.reshape import normalize_indices
 from typing_extensions import TypedDict
 
 from tests.operators.functionals.conftest import (
@@ -42,11 +43,11 @@ def test_functional_shape(functional: type[ElementaryFunctional], shape: torch.S
         # make all dimensions singleton
         expected_shape = (1,) * len(shape)
     elif not keepdim:
-        # remove the dimensions in dim
-        expected_shape = tuple([s for i, s in enumerate(shape) if i not in torch.tensor(dim) % len(shape)])
+        normalized = normalize_indices(len(shape), dim)
+        expected_shape = tuple(s for i, s in enumerate(shape) if i not in normalized)
     elif keepdim:
-        # make the dimensions in dim singleton
-        expected_shape = tuple([s if i not in torch.tensor(dim) % len(shape) else 1 for i, s in enumerate(shape)])
+        normalized = normalize_indices(len(shape), dim)
+        expected_shape = tuple(s if i not in normalized else 1 for i, s in enumerate(shape))
     assert fx.shape == expected_shape
 
 

fzimmermann89/mr2

src/mr2/operators/AveragingOp.py
@@ -6,6 +6,7 @@ from warnings import warn
 import torch
 
 from mr2.operators.LinearOperator import LinearOperator
+from mr2.utils.reshape import normalize_index
 
 
 class AveragingOp(LinearOperator):
@@ -104,8 +105,9 @@ class AveragingOp(LinearOperator):
             )
             self.domain_size = self._last_domain_size
 
-        adjoint = x.new_zeros(*x.shape[: self.dim], self.domain_size, *x.shape[self.dim + 1 :])
-        placeholder = (slice(None),) * (self.dim % x.ndim)
+        dim = normalize_index(x.ndim, self.dim)
+        adjoint = x.new_zeros(*x.shape[:dim], self.domain_size, *x.shape[dim + 1 :])
+        placeholder = (slice(None),) * dim
         for i, group in enumerate(self.idx):
             if isinstance(group, slice):
                 n = len(range(*group.indices(self.domain_size)))
@@ -115,7 +117,7 @@ class AveragingOp(LinearOperator):
                 n = len(group)
 
             adjoint[(*placeholder, group)] += (
-                x[(*placeholder, i, None)].expand(*x.shape[: self.dim], n, *x.shape[self.dim + 1 :]) / n
+                x[(*placeholder, i, None)].expand(*x.shape[:dim], n, *x.shape[dim + 1 :]) / n
             )
 
         return (adjoint,)

src/mr2/utils/fill_range.py
@@ -2,6 +2,8 @@
 
 import torch
 
+from mr2.utils.reshape import normalize_index
+
 
 def fill_range_(tensor: torch.Tensor, dim: int) -> None:
     """
@@ -15,10 +17,7 @@ def fill_range_(tensor: torch.Tensor, dim: int) -> None:
     dim
         The dimension along which to fill with increasing values.
     """
-    if not -tensor.ndim <= dim < tensor.ndim:
-        raise IndexError(f'Dimension {dim} is out of range for tensor with {tensor.ndim} dimensions.')
-
-    dim = dim % tensor.ndim
+    dim = normalize_index(tensor.ndim, dim)
     shape = [s if d == dim else 1 for d, s in enumerate(tensor.shape)]
     values = torch.arange(tensor.size(dim), device=tensor.device).reshape(shape)
     tensor[:] = values.expand_as(tensor)

src/mr2/utils/interpolate.py
@@ -5,6 +5,8 @@ from typing import Literal
 
 import torch
 
+from mr2.utils.reshape import normalize_indices
+
 
 def interpolate(
     x: torch.Tensor, size: Sequence[int], dim: Sequence[int], mode: Literal['nearest', 'linear'] = 'linear'
@@ -30,10 +32,7 @@ def interpolate(
     if len(dim) != len(size):
         raise ValueError('Must provide matching length size and dim arguments.')
 
-    # normalize dim to allow negative indexing in input
-    dim = tuple([a % x.ndim for a in dim])
-    if len(dim) != len(set(dim)):
-        raise ValueError(f'Dim must be unique. Normalized dims are {dim}')
+    dim = normalize_indices(x.ndim, dim)
 
     # return input tensor if old and new size match
     if all(x.shape[d] == s for s, d in zip(size, dim, strict=True)):

src/mr2/utils/pad_or_crop.py
@@ -6,8 +6,7 @@ from typing import Literal
 
 import torch
 
-from mr2.utils.indexing import normalize_index
-from mr2.utils.reshape import unsqueeze_left
+from mr2.utils.reshape import normalize_index, unsqueeze_left
 
 
 def pad_or_crop(

tests/operators/functionals/test_functionals.py
@@ -6,6 +6,7 @@ import torch
 from mr2.operators.Functional import ElementaryFunctional, ElementaryProximableFunctional
 from mr2.operators.functionals import MSE, L1Norm, L1NormViewAsReal, L2NormSquared, ZeroFunctional
 from mr2.utils import RandomGenerator
+from mr2.utils.reshape import normalize_indices
 from typing_extensions import TypedDict
 
 from tests.operators.functionals.conftest import (
@@ -42,11 +43,11 @@ def test_functional_shape(functional: type[ElementaryFunctional], shape: torch.S
         # make all dimensions singleton
         expected_shape = (1,) * len(shape)
     elif not keepdim:
-        # remove the dimensions in dim
-        expected_shape = tuple([s for i, s in enumerate(shape) if i not in torch.tensor(dim) % len(shape)])
+        normalized = normalize_indices(len(shape), dim)
+        expected_shape = tuple(s for i, s in enumerate(shape) if i not in normalized)
     elif keepdim:
-        # make the dimensions in dim singleton
-        expected_shape = tuple([s if i not in torch.tensor(dim) % len(shape) else 1 for i, s in enumerate(shape)])
+        normalized = normalize_indices(len(shape), dim)
+        expected_shape = tuple(s if i not in normalized else 1 for i, s in enumerate(shape))
     assert fx.shape == expected_shape
 
 