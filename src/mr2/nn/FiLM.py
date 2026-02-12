PTB-MR/mrpro

add core nn foundations, layers, and resize blocks
ghstack-source-id: 2572dcb021073d4cfae8e1bf8e4d7a8d5f58a661
ghstack-comment-id: 3865650347
Pull-Request-resolved: https://github.com/PTB-MR/mrpro/pull/953

src/mrpro/nn/FiLM.py
@@ -1,0 +1,68 @@
+"""Feature-wise Linear Modulation."""
+
+import torch
+from torch.nn import Linear, Module
+
+from mrpro.nn.CondMixin import CondMixin
+from mrpro.utils.reshape import unsqueeze_tensors_right
+
+
+class FiLM(CondMixin, Module):
+    """Feature-wise Linear Modulation.
+
+    Feature-wise Linear Modulation from [FiLM]_ to condition a network on a conditioning tensor.
+
+
+    References
+    ----------
+    ..[FiLM] Perez, L., Strub, F., de Vries, H., Dumoulin, V., & Courville, A. "FiLM : Visual reasoning with a general
+      conditioning layer." AAAI (2018). https://arxiv.org/abs/1709.07871
+    """
+
+    features_last: bool
+
+    def __init__(self, channels: int, cond_dim: int, features_last: bool = False) -> None:
+        """Initialize FiLM.
+
+        Parameters
+        ----------
+        channels
+            The number of channels in the input tensor.
+        cond_dim
+            The dimension of the conditioning tensor.
+        features_last
+            Whether the features are in the last dimension of the input tensor (e.g. transformer tokens)
+            or in the second dimension (e.g. image tensors).
+        """
+        super().__init__()
+        self.project = Linear(cond_dim, 2 * channels) if cond_dim > 0 else None
+        self.features_last = features_last
+
+    def __call__(self, x: torch.Tensor, *, cond: torch.Tensor | None = None) -> torch.Tensor:
+        """Apply FiLM.
+
+        Parameters
+        ----------
+        x
+            The input tensor.
+        cond
+            The conditioning tensor.
+        """
+        return super().__call__(x, cond=cond)
+
+    def forward(self, x: torch.Tensor, *, cond: torch.Tensor | None = None) -> torch.Tensor:
+        """Apply FiLM."""
+        if cond is None or self.project is None:
+            return x
+
+        if self.features_last:
+            x = x.moveaxis(-1, 1)
+
+        scale, shift = self.project(cond).chunk(2, dim=1)
+        scale, shift = unsqueeze_tensors_right(scale, shift, ndim=x.ndim)
+        x = x * (1 + scale) + shift
+
+        if self.features_last:
+            x = x.moveaxis(1, -1)
+
+        return x

src/mrpro/nn/Residual.py
@@ -1,0 +1,45 @@
+"""Residual connection."""
+
+import torch
+from torch.nn import Identity, Module
+
+from mrpro.nn.CondMixin import CondMixin, call_with_cond
+
+
+class Residual(CondMixin, Module):
+    """Residual connection."""
+
+    def __init__(self, module: Module, skip: Module | None = None):
+        """Initialize the residual connection.
+
+        Parameters
+        ----------
+        module
+            The main path of the residual connection.
+        skip
+            The skip path of the residual connection. If None, the identity function is used.
+        """
+        super().__init__()
+        self.module = module
+        self.skip = Identity() if skip is None else skip
+
+    def __call__(self, *x: torch.Tensor, cond: torch.Tensor | None = None) -> torch.Tensor:
+        """Apply the module.
+
+        Parameters
+        ----------
+        x
+            The input tensor.
+        cond
+            The optional conditioning tensor. If the modules are an instance of `CondMixin`,
+            the conditioning is passed to the modules.
+
+        Returns
+        -------
+            The output tensor.
+        """
+        return super().__call__(*x, cond=cond)
+
+    def forward(self, *x: torch.Tensor, cond: torch.Tensor | None = None) -> torch.Tensor:
+        """Apply the module."""
+        return call_with_cond(self.module, *x, cond=cond) + call_with_cond(self.skip, *x, cond=cond)

src/mrpro/nn/Sequential.py
@@ -1,0 +1,60 @@
+"""Sequential container with support for conditioning and Operators."""
+
+from collections import OrderedDict
+from typing import cast
+
+import torch
+
+from mrpro.nn.CondMixin import CondMixin
+from mrpro.operators import Operator
+
+
+class Sequential(CondMixin, torch.nn.Sequential):
+    """Sequential container with support for conditioning and Operators.
+
+    Allows multiple input tensors and a single output tensor of the sequential block.
+
+    """
+
+    def __call__(self, *x: torch.Tensor, cond: torch.Tensor | None = None) -> torch.Tensor:
+        """Apply all modules in series to the input.
+
+        Parameters
+        ----------
+        x
+            The input tensor.
+        cond
+            The (optional) conditioning tensor.
+
+        Returns
+        -------
+            The output tensor.
+        """
+        return torch.nn.Sequential.__call__(self, *x, cond=cond)
+
+    def forward(self, *x: torch.Tensor, cond: torch.Tensor | None = None) -> torch.Tensor:
+        """Apply all modules in series to the input."""
+        for module in self:
+            if isinstance(module, Operator):
+                x = cast(tuple[torch.Tensor, ...], module(*x))  # always tuple
+            else:
+                ret: torch.Tensor | tuple[torch.Tensor, ...]
+                if isinstance(module, CondMixin):
+                    ret = module(*x, cond=cond)
+                else:
+                    ret = module(*x)
+                if isinstance(ret, tuple):
+                    x = ret
+                else:
+                    x = (ret,)
+        return x[0]
+
+    def __getitem__(self, idx: slice | int) -> 'Sequential':
+        """Get a slice or item from the Sequential container.
+
+        Subclasses will decompose to `Sequential` on indexing.
+        """
+        if isinstance(idx, slice):
+            return Sequential(OrderedDict(list(self._modules.items())[idx]))
+        else:
+            return cast(Sequential, self._get_item_by_idx(self._modules.values(), idx))

tests/nn/test_ndmodules.py
@@ -1,0 +1,76 @@
+"""Tests for the ndmodules module."""
+
+import pytest
+import torch
+from mrpro.nn.ndmodules import (
+    adaptiveAvgPoolND,
+    avgPoolND,
+    batchNormND,
+    convND,
+    convTransposeND,
+    instanceNormND,
+    maxPoolND,
+)
+
+
+def test_convnd() -> None:
+    """Test ConvND."""
+    assert convND(1) is torch.nn.Conv1d
+    assert convND(2) is torch.nn.Conv2d
+    assert convND(3) is torch.nn.Conv3d
+    with pytest.raises(NotImplementedError):
+        convND(4)
+
+
+def test_convtransposend() -> None:
+    """Test ConvTransposeND."""
+    assert convTransposeND(1) is torch.nn.ConvTranspose1d
+    assert convTransposeND(2) is torch.nn.ConvTranspose2d
+    assert convTransposeND(3) is torch.nn.ConvTranspose3d
+    with pytest.raises(NotImplementedError):
+        convTransposeND(4)
+
+
+def test_maxpoolnd() -> None:
+    """Test MaxPoolND."""
+    assert maxPoolND(1) is torch.nn.MaxPool1d
+    assert maxPoolND(2) is torch.nn.MaxPool2d
+    assert maxPoolND(3) is torch.nn.MaxPool3d
+    with pytest.raises(NotImplementedError):
+        maxPoolND(4)
+
+
+def test_avgpoolnd() -> None:
+    """Test AvgPoolND."""
+    assert avgPoolND(1) is torch.nn.AvgPool1d
+    assert avgPoolND(2) is torch.nn.AvgPool2d
+    assert avgPoolND(3) is torch.nn.AvgPool3d
+    with pytest.raises(NotImplementedError):
+        avgPoolND(4)
+
+
+def test_adaptiveavgpoolnd() -> None:
+    """Test AdaptiveAvgPoolND."""
+    assert adaptiveAvgPoolND(1) is torch.nn.AdaptiveAvgPool1d
+    assert adaptiveAvgPoolND(2) is torch.nn.AdaptiveAvgPool2d
+    assert adaptiveAvgPoolND(3) is torch.nn.AdaptiveAvgPool3d
+    with pytest.raises(NotImplementedError):
+        adaptiveAvgPoolND(4)
+
+
+def test_instancenormnd() -> None:
+    """Test InstanceNormND."""
+    assert instanceNormND(1) is torch.nn.InstanceNorm1d
+    assert instanceNormND(2) is torch.nn.InstanceNorm2d
+    assert instanceNormND(3) is torch.nn.InstanceNorm3d
+    with pytest.raises(NotImplementedError):
+        instanceNormND(4)
+
+
+def test_batchnormnd() -> None:
+    """Test BatchNormND."""
+    assert batchNormND(1) is torch.nn.BatchNorm1d
+    assert batchNormND(2) is torch.nn.BatchNorm2d
+    assert batchNormND(3) is torch.nn.BatchNorm3d
+    with pytest.raises(NotImplementedError):
+        batchNormND(4)

tests/nn/test_pixelshuffle.py
@@ -1,0 +1,92 @@
+"""Test PixelShuffle and PixelUnshuffle."""
+
+from typing import cast
+
+import torch
+from mrpro.nn.PixelShuffle import PixelShuffle, PixelShuffleUpsample, PixelUnshuffle, PixelUnshuffleDownsample
+from mrpro.utils import RandomGenerator
+
+
+def test_pixel_shuffle_2d() -> None:
+    """Test PixelUnshuffle's fast path for 2D images."""
+    x = torch.arange(3 * 4 * 8).reshape(1, 3, 4, 8)
+    pixel_unshuffle = PixelUnshuffle(2)
+    y = pixel_unshuffle(x)
+    assert y.shape == (1, 3 * 4, 4 // 2, 8 // 2)
+
+    pixel_shuffle = PixelShuffle(2)
+    z = pixel_shuffle(y)
+    assert z.shape == (1, 3, 4, 8)
+    assert (x == z).all()
+
+
+def test_pixel_unshuffle_4d() -> None:
+    """Test PixelUnshuffle's general case."""
+    x = torch.arange(3 * 4 * 8 * 10 * 12).reshape(1, 3, 4, 8, 10, 12)
+    pixel_unshuffle = PixelUnshuffle(2)
+    y = pixel_unshuffle(x)
+    assert y.shape == (1, 3 * 16, 4 // 2, 8 // 2, 10 // 2, 12 // 2)
+
+    pixel_shuffle = PixelShuffle(2)
+    z = pixel_shuffle(y)
+    assert z.shape == (1, 3, 4, 8, 10, 12)
+    assert (x == z).all()
+
+
+def test_pixelunshuffle_features_last() -> None:
+    """Test PixelUnshuffle with features_last."""
+    x = torch.arange(3 * 4 * 8 * 10 * 12).reshape(1, 3, 4, 8, 10, 12)
+    pixel_unshuffle_last = PixelUnshuffle(2, features_last=True)
+    pixel_unshuffle = PixelUnshuffle(2, features_last=False)
+    y_last = pixel_unshuffle_last(x.moveaxis(1, -1)).moveaxis(-1, 1)
+    y_normal = pixel_unshuffle(x)
+    assert (y_last == y_normal).all()
+
+
+def test_pixelshuffle_features_last() -> None:
+    """Test PixelShuffle with features_last."""
+    x = torch.arange(3 * 4 * 8 * 10 * 12).reshape(1, -1, 2, 4, 5, 6)
+    pixel_shuffle_last = PixelShuffle(2, features_last=True)
+    pixel_shuffle = PixelShuffle(2, features_last=False)
+    y_last = pixel_shuffle_last(x.moveaxis(1, -1)).moveaxis(-1, 1)
+    y_normal = pixel_shuffle(x)
+    assert (y_last == y_normal).all()
+
+
+def test_unpixelshuffledownsample_residual() -> None:
+    """Test PixelUnshuffleDownsample with residual."""
+    rng = RandomGenerator(seed=42)
+    x = rng.float32_tensor((1, 2, 9, 12, 15))
+    downsample = PixelUnshuffleDownsample(3, 2, 27, downscale_factor=3, residual=True)
+    y = downsample(x)
+    assert y.shape == (1, 27, 3, 4, 5)
+
+
+def test_pixelshuffleupsample_residual() -> None:
+    """Test PixelShuffleUpsample with residual."""
+    rng = RandomGenerator(seed=42)
+    x = rng.float32_tensor((1, 2, 3, 4, 5))
+    upsample = PixelShuffleUpsample(3, 2, 1, upscale_factor=3, residual=True)
+    y = upsample(x)
+    assert y.shape == (1, 1, 9, 12, 15)
+
+
+def test_pixelshuffleupsample_pixelunshuffledownsample() -> None:
+    """Test if PixelUnshuffleDownsample is the inverse of PixelShuffleUpsample."""
+    rng = RandomGenerator(seed=42)
+    x = rng.float32_tensor((1, 3**3, 3, 4, 5))
+    # Only without residual, the upsample and downsample are inverses.
+    downsample = PixelUnshuffleDownsample(3, 1, 3**3, downscale_factor=3, residual=False)
+    upsample = PixelShuffleUpsample(3, 3**3, 1, upscale_factor=3, residual=False)
+    # Only if the convs are Identity, the upsample and downsample are inverses.
+    torch.nn.init.dirac_(cast(torch.Tensor, downsample.projection.weight))
+    torch.nn.init.dirac_(cast(torch.Tensor, upsample.projection.weight))
+    downsample_bias = cast(torch.Tensor | None, downsample.projection.bias)
+    upsample_bias = cast(torch.Tensor | None, upsample.projection.bias)
+    if downsample_bias is not None:
+        torch.nn.init.zeros_(downsample_bias)
+    if upsample_bias is not None:
+        torch.nn.init.zeros_(upsample_bias)
+    y = downsample(upsample(x))
+    assert y.shape == (1, 3**3, 3, 4, 5)
+    torch.testing.assert_close(y, x, msg='Upsample and downsample are not inverses.')

fzimmermann89/mr2

src/mr2/nn/FiLM.py
@@ -1,0 +1,68 @@
+"""Feature-wise Linear Modulation."""
+
+import torch
+from torch.nn import Linear, Module
+
+from mr2.nn.CondMixin import CondMixin
+from mr2.utils.reshape import unsqueeze_tensors_right
+
+
+class FiLM(CondMixin, Module):
+    """Feature-wise Linear Modulation.
+
+    Feature-wise Linear Modulation from [FiLM]_ to condition a network on a conditioning tensor.
+
+
+    References
+    ----------
+    ..[FiLM] Perez, L., Strub, F., de Vries, H., Dumoulin, V., & Courville, A. "FiLM : Visual reasoning with a general
+      conditioning layer." AAAI (2018). https://arxiv.org/abs/1709.07871
+    """
+
+    features_last: bool
+
+    def __init__(self, channels: int, cond_dim: int, features_last: bool = False) -> None:
+        """Initialize FiLM.
+
+        Parameters
+        ----------
+        channels
+            The number of channels in the input tensor.
+        cond_dim
+            The dimension of the conditioning tensor.
+        features_last
+            Whether the features are in the last dimension of the input tensor (e.g. transformer tokens)
+            or in the second dimension (e.g. image tensors).
+        """
+        super().__init__()
+        self.project = Linear(cond_dim, 2 * channels) if cond_dim > 0 else None
+        self.features_last = features_last
+
+    def __call__(self, x: torch.Tensor, *, cond: torch.Tensor | None = None) -> torch.Tensor:
+        """Apply FiLM.
+
+        Parameters
+        ----------
+        x
+            The input tensor.
+        cond
+            The conditioning tensor.
+        """
+        return super().__call__(x, cond=cond)
+
+    def forward(self, x: torch.Tensor, *, cond: torch.Tensor | None = None) -> torch.Tensor:
+        """Apply FiLM."""
+        if cond is None or self.project is None:
+            return x
+
+        if self.features_last:
+            x = x.moveaxis(-1, 1)
+
+        scale, shift = self.project(cond).chunk(2, dim=1)
+        scale, shift = unsqueeze_tensors_right(scale, shift, ndim=x.ndim)
+        x = x * (1 + scale) + shift
+
+        if self.features_last:
+            x = x.moveaxis(1, -1)
+
+        return x

src/mr2/nn/Residual.py
@@ -1,0 +1,45 @@
+"""Residual connection."""
+
+import torch
+from torch.nn import Identity, Module
+
+from mr2.nn.CondMixin import CondMixin, call_with_cond
+
+
+class Residual(CondMixin, Module):
+    """Residual connection."""
+
+    def __init__(self, module: Module, skip: Module | None = None):
+        """Initialize the residual connection.
+
+        Parameters
+        ----------
+        module
+            The main path of the residual connection.
+        skip
+            The skip path of the residual connection. If None, the identity function is used.
+        """
+        super().__init__()
+        self.module = module
+        self.skip = Identity() if skip is None else skip
+
+    def __call__(self, *x: torch.Tensor, cond: torch.Tensor | None = None) -> torch.Tensor:
+        """Apply the module.
+
+        Parameters
+        ----------
+        x
+            The input tensor.
+        cond
+            The optional conditioning tensor. If the modules are an instance of `CondMixin`,
+            the conditioning is passed to the modules.
+
+        Returns
+        -------
+            The output tensor.
+        """
+        return super().__call__(*x, cond=cond)
+
+    def forward(self, *x: torch.Tensor, cond: torch.Tensor | None = None) -> torch.Tensor:
+        """Apply the module."""
+        return call_with_cond(self.module, *x, cond=cond) + call_with_cond(self.skip, *x, cond=cond)

src/mr2/nn/Sequential.py
@@ -1,0 +1,60 @@
+"""Sequential container with support for conditioning and Operators."""
+
+from collections import OrderedDict
+from typing import cast
+
+import torch
+
+from mr2.nn.CondMixin import CondMixin
+from mr2.operators import Operator
+
+
+class Sequential(CondMixin, torch.nn.Sequential):
+    """Sequential container with support for conditioning and Operators.
+
+    Allows multiple input tensors and a single output tensor of the sequential block.
+
+    """
+
+    def __call__(self, *x: torch.Tensor, cond: torch.Tensor | None = None) -> torch.Tensor:
+        """Apply all modules in series to the input.
+
+        Parameters
+        ----------
+        x
+            The input tensor.
+        cond
+            The (optional) conditioning tensor.
+
+        Returns
+        -------
+            The output tensor.
+        """
+        return torch.nn.Sequential.__call__(self, *x, cond=cond)
+
+    def forward(self, *x: torch.Tensor, cond: torch.Tensor | None = None) -> torch.Tensor:
+        """Apply all modules in series to the input."""
+        for module in self:
+            if isinstance(module, Operator):
+                x = cast(tuple[torch.Tensor, ...], module(*x))  # always tuple
+            else:
+                ret: torch.Tensor | tuple[torch.Tensor, ...]
+                if isinstance(module, CondMixin):
+                    ret = module(*x, cond=cond)
+                else:
+                    ret = module(*x)
+                if isinstance(ret, tuple):
+                    x = ret
+                else:
+                    x = (ret,)
+        return x[0]
+
+    def __getitem__(self, idx: slice | int) -> 'Sequential':
+        """Get a slice or item from the Sequential container.
+
+        Subclasses will decompose to `Sequential` on indexing.
+        """
+        if isinstance(idx, slice):
+            return Sequential(OrderedDict(list(self._modules.items())[idx]))
+        else:
+            return cast(Sequential, self._get_item_by_idx(self._modules.values(), idx))

tests/nn/test_ndmodules.py
@@ -1,0 +1,76 @@
+"""Tests for the ndmodules module."""
+
+import pytest
+import torch
+from mr2.nn.ndmodules import (
+    adaptiveAvgPoolND,
+    avgPoolND,
+    batchNormND,
+    convND,
+    convTransposeND,
+    instanceNormND,
+    maxPoolND,
+)
+
+
+def test_convnd() -> None:
+    """Test ConvND."""
+    assert convND(1) is torch.nn.Conv1d
+    assert convND(2) is torch.nn.Conv2d
+    assert convND(3) is torch.nn.Conv3d
+    with pytest.raises(NotImplementedError):
+        convND(4)
+
+
+def test_convtransposend() -> None:
+    """Test ConvTransposeND."""
+    assert convTransposeND(1) is torch.nn.ConvTranspose1d
+    assert convTransposeND(2) is torch.nn.ConvTranspose2d
+    assert convTransposeND(3) is torch.nn.ConvTranspose3d
+    with pytest.raises(NotImplementedError):
+        convTransposeND(4)
+
+
+def test_maxpoolnd() -> None:
+    """Test MaxPoolND."""
+    assert maxPoolND(1) is torch.nn.MaxPool1d
+    assert maxPoolND(2) is torch.nn.MaxPool2d
+    assert maxPoolND(3) is torch.nn.MaxPool3d
+    with pytest.raises(NotImplementedError):
+        maxPoolND(4)
+
+
+def test_avgpoolnd() -> None:
+    """Test AvgPoolND."""
+    assert avgPoolND(1) is torch.nn.AvgPool1d
+    assert avgPoolND(2) is torch.nn.AvgPool2d
+    assert avgPoolND(3) is torch.nn.AvgPool3d
+    with pytest.raises(NotImplementedError):
+        avgPoolND(4)
+
+
+def test_adaptiveavgpoolnd() -> None:
+    """Test AdaptiveAvgPoolND."""
+    assert adaptiveAvgPoolND(1) is torch.nn.AdaptiveAvgPool1d
+    assert adaptiveAvgPoolND(2) is torch.nn.AdaptiveAvgPool2d
+    assert adaptiveAvgPoolND(3) is torch.nn.AdaptiveAvgPool3d
+    with pytest.raises(NotImplementedError):
+        adaptiveAvgPoolND(4)
+
+
+def test_instancenormnd() -> None:
+    """Test InstanceNormND."""
+    assert instanceNormND(1) is torch.nn.InstanceNorm1d
+    assert instanceNormND(2) is torch.nn.InstanceNorm2d
+    assert instanceNormND(3) is torch.nn.InstanceNorm3d
+    with pytest.raises(NotImplementedError):
+        instanceNormND(4)
+
+
+def test_batchnormnd() -> None:
+    """Test BatchNormND."""
+    assert batchNormND(1) is torch.nn.BatchNorm1d
+    assert batchNormND(2) is torch.nn.BatchNorm2d
+    assert batchNormND(3) is torch.nn.BatchNorm3d
+    with pytest.raises(NotImplementedError):
+        batchNormND(4)

tests/nn/test_pixelshuffle.py
@@ -1,0 +1,92 @@
+"""Test PixelShuffle and PixelUnshuffle."""
+
+from typing import cast
+
+import torch
+from mr2.nn.PixelShuffle import PixelShuffle, PixelShuffleUpsample, PixelUnshuffle, PixelUnshuffleDownsample
+from mr2.utils import RandomGenerator
+
+
+def test_pixel_shuffle_2d() -> None:
+    """Test PixelUnshuffle's fast path for 2D images."""
+    x = torch.arange(3 * 4 * 8).reshape(1, 3, 4, 8)
+    pixel_unshuffle = PixelUnshuffle(2)
+    y = pixel_unshuffle(x)
+    assert y.shape == (1, 3 * 4, 4 // 2, 8 // 2)
+
+    pixel_shuffle = PixelShuffle(2)
+    z = pixel_shuffle(y)
+    assert z.shape == (1, 3, 4, 8)
+    assert (x == z).all()
+
+
+def test_pixel_unshuffle_4d() -> None:
+    """Test PixelUnshuffle's general case."""
+    x = torch.arange(3 * 4 * 8 * 10 * 12).reshape(1, 3, 4, 8, 10, 12)
+    pixel_unshuffle = PixelUnshuffle(2)
+    y = pixel_unshuffle(x)
+    assert y.shape == (1, 3 * 16, 4 // 2, 8 // 2, 10 // 2, 12 // 2)
+
+    pixel_shuffle = PixelShuffle(2)
+    z = pixel_shuffle(y)
+    assert z.shape == (1, 3, 4, 8, 10, 12)
+    assert (x == z).all()
+
+
+def test_pixelunshuffle_features_last() -> None:
+    """Test PixelUnshuffle with features_last."""
+    x = torch.arange(3 * 4 * 8 * 10 * 12).reshape(1, 3, 4, 8, 10, 12)
+    pixel_unshuffle_last = PixelUnshuffle(2, features_last=True)
+    pixel_unshuffle = PixelUnshuffle(2, features_last=False)
+    y_last = pixel_unshuffle_last(x.moveaxis(1, -1)).moveaxis(-1, 1)
+    y_normal = pixel_unshuffle(x)
+    assert (y_last == y_normal).all()
+
+
+def test_pixelshuffle_features_last() -> None:
+    """Test PixelShuffle with features_last."""
+    x = torch.arange(3 * 4 * 8 * 10 * 12).reshape(1, -1, 2, 4, 5, 6)
+    pixel_shuffle_last = PixelShuffle(2, features_last=True)
+    pixel_shuffle = PixelShuffle(2, features_last=False)
+    y_last = pixel_shuffle_last(x.moveaxis(1, -1)).moveaxis(-1, 1)
+    y_normal = pixel_shuffle(x)
+    assert (y_last == y_normal).all()
+
+
+def test_unpixelshuffledownsample_residual() -> None:
+    """Test PixelUnshuffleDownsample with residual."""
+    rng = RandomGenerator(seed=42)
+    x = rng.float32_tensor((1, 2, 9, 12, 15))
+    downsample = PixelUnshuffleDownsample(3, 2, 27, downscale_factor=3, residual=True)
+    y = downsample(x)
+    assert y.shape == (1, 27, 3, 4, 5)
+
+
+def test_pixelshuffleupsample_residual() -> None:
+    """Test PixelShuffleUpsample with residual."""
+    rng = RandomGenerator(seed=42)
+    x = rng.float32_tensor((1, 2, 3, 4, 5))
+    upsample = PixelShuffleUpsample(3, 2, 1, upscale_factor=3, residual=True)
+    y = upsample(x)
+    assert y.shape == (1, 1, 9, 12, 15)
+
+
+def test_pixelshuffleupsample_pixelunshuffledownsample() -> None:
+    """Test if PixelUnshuffleDownsample is the inverse of PixelShuffleUpsample."""
+    rng = RandomGenerator(seed=42)
+    x = rng.float32_tensor((1, 3**3, 3, 4, 5))
+    # Only without residual, the upsample and downsample are inverses.
+    downsample = PixelUnshuffleDownsample(3, 1, 3**3, downscale_factor=3, residual=False)
+    upsample = PixelShuffleUpsample(3, 3**3, 1, upscale_factor=3, residual=False)
+    # Only if the convs are Identity, the upsample and downsample are inverses.
+    torch.nn.init.dirac_(cast(torch.Tensor, downsample.projection.weight))
+    torch.nn.init.dirac_(cast(torch.Tensor, upsample.projection.weight))
+    downsample_bias = cast(torch.Tensor | None, downsample.projection.bias)
+    upsample_bias = cast(torch.Tensor | None, upsample.projection.bias)
+    if downsample_bias is not None:
+        torch.nn.init.zeros_(downsample_bias)
+    if upsample_bias is not None:
+        torch.nn.init.zeros_(upsample_bias)
+    y = downsample(upsample(x))
+    assert y.shape == (1, 3**3, 3, 4, 5)
+    torch.testing.assert_close(y, x, msg='Upsample and downsample are not inverses.')