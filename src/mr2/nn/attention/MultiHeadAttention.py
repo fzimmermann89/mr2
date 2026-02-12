PTB-MR/mrpro

add positional encodings and attention modules
ghstack-source-id: d9a4c723705eca4a10ffefd1351311375fb9a767
ghstack-comment-id: 3865650808
Pull-Request-resolved: https://github.com/PTB-MR/mrpro/pull/955

src/mrpro/nn/attention/MultiHeadAttention.py
@@ -1,0 +1,106 @@
+"""Multi-head Attention."""
+
+import torch
+from einops import rearrange
+from torch.nn import Linear, Module
+
+from mrpro.nn.AxialRoPE import AxialRoPE
+
+
+class MultiHeadAttention(Module):
+    """Multi-head Attention.
+
+    Implements multihead scaled dot-product attention and supports "image-like" inputs,
+    i.e. `batch, channels, *spatial_dims` as well as "transformer-like" inputs, `batch, sequence, features`.
+    """
+
+    def __init__(
+        self,
+        n_channels_in: int,
+        n_channels_out: int,
+        n_heads: int,
+        features_last: bool = False,
+        p_dropout: float = 0.0,
+        n_channels_cross: int | None = None,
+        rope_embed_fraction: float = 0.0,
+    ):
+        """Initialize the Multi-head Attention.
+
+        Parameters
+        ----------
+        n_channels_in
+            Number of input channels.
+        n_channels_out
+            Number of output channels.
+        n_heads
+            number of attention heads
+        features_last
+            Whether the features dimension is the last dimension, as common in transformer models,
+            or the second dimension, as common in image models.
+        p_dropout
+            Dropout probability.
+        n_channels_cross
+            Number of channels for cross-attention. If `None`, use `n_channels_in`.
+        rope_embed_fraction
+            Fraction of channels to embed with RoPE.
+        """
+        super().__init__()
+        n_channels_kv = n_channels_cross if n_channels_cross is not None else n_channels_in
+        channels_per_head_q = n_channels_in // n_heads
+        channels_per_head_kv = n_channels_kv // n_heads
+        self.to_q = Linear(n_channels_in, channels_per_head_q * n_heads)
+        self.to_kv = Linear(n_channels_kv, channels_per_head_kv * n_heads * 2)
+        self.p_dropout = p_dropout
+        self.features_last = features_last
+        self.to_out = Linear(n_channels_in, n_channels_out)
+        self.n_heads = n_heads
+        self.rope = AxialRoPE(rope_embed_fraction)
+
+    def __call__(self, x: torch.Tensor, cross_attention: torch.Tensor | None = None) -> torch.Tensor:
+        """Apply multi-head attention.
+
+        Parameters
+        ----------
+        x
+            The input tensor.
+        cross_attention
+            The key and value tensors for cross-attention. If `None`, self-attention is applied.
+
+        Returns
+        -------
+            The output tensor.
+        """
+        return super().__call__(x, cross_attention)
+
+    def _reshape(self, x: torch.Tensor) -> torch.Tensor:
+        if not self.features_last:
+            x = x.moveaxis(1, -1)
+        return x.flatten(1, -2)
+
+    def forward(self, x: torch.Tensor, cross_attention: torch.Tensor | None = None) -> torch.Tensor:
+        """Apply multi-head attention."""
+        if cross_attention is None:
+            cross_attention = x
+        if not self.features_last:
+            x = x.moveaxis(1, -1)
+            cross_attention = cross_attention.moveaxis(1, -1)
+
+        query = rearrange(self.to_q(x), 'batch ... (heads channels) -> batch heads ... channels ', heads=self.n_heads)
+        key, value = rearrange(
+            self.to_kv(cross_attention),
+            'batch ... (kv heads channels) -> kv batch heads ... channels ',
+            heads=self.n_heads,
+            kv=2,
+        )
+        query, key = self.rope(query, key)  # NO-OP if rope_embed_fraction is 0.0
+        query, key, value = query.flatten(2, -2), key.flatten(2, -2), value.flatten(2, -2)
+        y = torch.nn.functional.scaled_dot_product_attention(
+            query, key, value, dropout_p=self.p_dropout, is_causal=False
+        )
+        y = rearrange(y, '... heads L channels -> ... L (heads channels)')
+        out = self.to_out(y).reshape(x.shape)
+
+        if not self.features_last:
+            out = out.moveaxis(-1, 1)
+
+        return out

src/mrpro/nn/attention/SqueezeExcitation.py
@@ -1,0 +1,57 @@
+"""Squeeze-and-Excitation block."""
+
+import torch
+from torch.nn import Module, ReLU, Sigmoid
+
+from mrpro.nn.ndmodules import adaptiveAvgPoolND, convND
+from mrpro.nn.Sequential import Sequential
+
+
+class SqueezeExcitation(Module):
+    """Squeeze-and-Excitation block.
+
+    Sequeeze-and-Excitation block from [SE]_.
+
+    References
+    ----------
+    ..[SE] Hu, Jie, Li Shen, and Gang Sun. "Squeeze-and-excitation networks." CVPR 2018, https://arxiv.org/abs/1709.01507
+    """
+
+    def __init__(self, n_dim: int, n_channels_input: int, n_channels_squeeze: int) -> None:
+        """Initialize SqueezeExcitation.
+
+        Parameters
+        ----------
+        n_dim
+            The dimension of the input tensor.
+        n_channels_input
+            The number of channels in the input tensor.
+        n_channels_squeeze
+            The number of channels in the squeeze tensor.
+        """
+        super().__init__()
+        self.scale = Sequential(
+            adaptiveAvgPoolND(n_dim)(1),
+            convND(n_dim)(n_channels_input, n_channels_squeeze, kernel_size=1),
+            ReLU(),
+            convND(n_dim)(n_channels_squeeze, n_channels_input, kernel_size=1),
+            Sigmoid(),
+        )
+
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        """Apply SqueezeExcitation.
+
+        Parameters
+        ----------
+        x
+            The input tensor.
+
+        Returns
+        -------
+            The output tensor.
+        """
+        return super().__call__(x)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Apply SqueezeExcitation."""
+        return x * self.scale(x)

tests/nn/test_linearselfattention.py
@@ -1,0 +1,58 @@
+"""Tests for LinearSelfAttention module."""
+
+import pytest
+from mrpro.nn.attention import LinearSelfAttention
+from mrpro.utils import RandomGenerator
+
+
+@pytest.mark.parametrize(
+    'device',
+    [
+        pytest.param('cpu', id='cpu'),
+        pytest.param('cuda', id='cuda', marks=pytest.mark.cuda),
+    ],
+)
+@pytest.mark.parametrize(
+    ('n_channels_in', 'n_channels_out', 'n_heads', 'input_shape', 'features_last'),
+    [
+        (32, 32, 4, (1, 32, 32, 32), False),
+        (64, 64, 8, (2, 64, 16, 16), False),
+        (16, 16, 2, (1, 16, 16, 16), True),
+    ],
+)
+def test_linear_self_attention(
+    n_channels_in: int,
+    n_channels_out: int,
+    n_heads: int,
+    input_shape: tuple[int, ...],
+    features_last: bool,
+    device: str,
+) -> None:
+    """Test LinearSelfAttention output shape and backpropagation."""
+    rng = RandomGenerator(seed=42)
+    x = rng.float32_tensor(input_shape).to(device).requires_grad_(True)
+
+    attn = LinearSelfAttention(
+        n_channels_in=n_channels_in,
+        n_channels_out=n_channels_out,
+        n_heads=n_heads,
+        features_last=features_last,
+    ).to(device)
+
+    if features_last:
+        output = attn(x.moveaxis(1, -1)).moveaxis(-1, 1)
+    else:
+        output = attn(x)
+
+    expected_shape = (x.shape[0], n_channels_out, *x.shape[2:])
+    assert output.shape == expected_shape, f'Output shape {output.shape} != expected shape {expected_shape}'
+    assert not output.isnan().any(), 'NaN values in output'
+
+    output.sum().backward()
+    assert x.grad is not None, 'No gradient computed for input'
+    assert not x.grad.isnan().any(), 'NaN values in input gradients'
+
+    assert attn.to_qkv.weight.grad is not None, 'No gradient computed for to_qkv.weight'
+    assert attn.to_qkv.bias.grad is not None, 'No gradient computed for to_qkv.bias'
+    assert attn.to_out.weight.grad is not None, 'No gradient computed for to_out.weight'
+    assert attn.to_out.bias.grad is not None, 'No gradient computed for to_out.bias'

tests/nn/test_shiftedwindowattention.py
@@ -1,0 +1,61 @@
+"""Tests for ShiftedWindowAttention module."""
+
+import pytest
+from mrpro.nn.attention import ShiftedWindowAttention
+from mrpro.utils import RandomGenerator
+
+
+@pytest.mark.parametrize(
+    'device',
+    [
+        pytest.param('cpu', id='cpu'),
+        pytest.param('cuda', id='cuda', marks=pytest.mark.cuda),
+    ],
+)
+@pytest.mark.parametrize(
+    ('n_dim', 'window_size', 'shifted'),
+    [
+        (2, 8, False),
+        (4, 4, True),
+    ],
+)
+def test_shifted_window_attention(n_dim: int, window_size: int, shifted: bool, device: str) -> None:
+    """Test ShiftedWindowAttention output shape and backpropagation."""
+    n_batch, n_channels, n_heads = 2, 8, 2
+    spatial_shape = (window_size * 4,) * n_dim
+    rng = RandomGenerator(13)
+    x = rng.float32_tensor((n_batch, n_channels, *spatial_shape)).to(device).requires_grad_(True)
+    swin = ShiftedWindowAttention(
+        n_dim=n_dim,
+        n_channels_in=n_channels,
+        n_channels_out=n_channels,
+        n_heads=n_heads,
+        window_size=window_size,
+        shifted=shifted,
+    ).to(device)
+    out = swin(x)
+    assert out.shape == x.shape, f'Output shape {out.shape} != input shape {x.shape}'
+    assert not out.isnan().any(), 'NaN values in output'
+    out.sum().backward()
+    assert x.grad is not None, 'No gradient computed for input'
+    assert not x.grad.isnan().any(), 'NaN values in input gradients'
+    assert swin.to_qkv.weight.grad is not None, 'No gradient computed for to_qkv.weight'
+    assert swin.relative_position_bias_table.grad is not None, 'No gradient computed for relative_position_bias_table'
+
+
+@pytest.mark.parametrize('shifted', [True, False], ids=['shifted', 'non-shifted'])
+def test_shifted_window_attention_size_mismatch(shifted: bool):
+    n_batch, n_channels, n_heads, n_dim, window_size = 3, 4, 2, 2, 7
+    spatial_shape = (window_size * 4 + 1,) * n_dim
+    rng = RandomGenerator(13)
+    x = rng.float32_tensor((n_batch, n_channels, *spatial_shape))
+    swin = ShiftedWindowAttention(
+        n_dim=n_dim,
+        n_channels_in=n_channels,
+        n_channels_out=n_channels,
+        n_heads=n_heads,
+        window_size=window_size,
+        shifted=shifted,
+    )
+    out = swin(x)
+    assert out.shape == x.shape, f'Output shape {out.shape} != input shape {x.shape}'

tests/nn/test_squeezeexcitation.py
@@ -1,0 +1,32 @@
+"""Tests for SqueezeExcitation module."""
+
+from collections.abc import Sequence
+
+import pytest
+from mrpro.nn.attention import SqueezeExcitation
+from mrpro.utils import RandomGenerator
+
+
+@pytest.mark.parametrize(
+    ('dim', 'input_shape', 'squeeze_channels'),
+    [
+        (2, (1, 64, 32, 32), 16),
+        (3, (1, 64, 16, 16, 16), 16),
+    ],
+)
+def test_squeeze_excitation(
+    dim: int,
+    input_shape: Sequence[int],
+    squeeze_channels: int,
+) -> None:
+    """Test SqueezeExcitation output shape and backpropagation."""
+    rng = RandomGenerator(seed=42)
+    x = rng.float32_tensor(input_shape).requires_grad_(True)
+    se = SqueezeExcitation(n_dim=dim, n_channels_input=input_shape[1], n_channels_squeeze=squeeze_channels)
+    output = se(x)
+    assert output.shape == x.shape, f'Output shape {output.shape} != input shape {x.shape}'
+    output.sum().backward()
+    assert x.grad is not None, 'No gradient computed for input'
+    assert not output.isnan().any(), 'NaN values in output'
+    assert not x.grad.isnan().any(), 'NaN values in input gradients'
+    assert se.scale[1].weight.grad is not None, 'No gradient computed for Conv'

fzimmermann89/mr2

src/mr2/nn/attention/MultiHeadAttention.py
@@ -1,0 +1,106 @@
+"""Multi-head Attention."""
+
+import torch
+from einops import rearrange
+from torch.nn import Linear, Module
+
+from mr2.nn.AxialRoPE import AxialRoPE
+
+
+class MultiHeadAttention(Module):
+    """Multi-head Attention.
+
+    Implements multihead scaled dot-product attention and supports "image-like" inputs,
+    i.e. `batch, channels, *spatial_dims` as well as "transformer-like" inputs, `batch, sequence, features`.
+    """
+
+    def __init__(
+        self,
+        n_channels_in: int,
+        n_channels_out: int,
+        n_heads: int,
+        features_last: bool = False,
+        p_dropout: float = 0.0,
+        n_channels_cross: int | None = None,
+        rope_embed_fraction: float = 0.0,
+    ):
+        """Initialize the Multi-head Attention.
+
+        Parameters
+        ----------
+        n_channels_in
+            Number of input channels.
+        n_channels_out
+            Number of output channels.
+        n_heads
+            number of attention heads
+        features_last
+            Whether the features dimension is the last dimension, as common in transformer models,
+            or the second dimension, as common in image models.
+        p_dropout
+            Dropout probability.
+        n_channels_cross
+            Number of channels for cross-attention. If `None`, use `n_channels_in`.
+        rope_embed_fraction
+            Fraction of channels to embed with RoPE.
+        """
+        super().__init__()
+        n_channels_kv = n_channels_cross if n_channels_cross is not None else n_channels_in
+        channels_per_head_q = n_channels_in // n_heads
+        channels_per_head_kv = n_channels_kv // n_heads
+        self.to_q = Linear(n_channels_in, channels_per_head_q * n_heads)
+        self.to_kv = Linear(n_channels_kv, channels_per_head_kv * n_heads * 2)
+        self.p_dropout = p_dropout
+        self.features_last = features_last
+        self.to_out = Linear(n_channels_in, n_channels_out)
+        self.n_heads = n_heads
+        self.rope = AxialRoPE(rope_embed_fraction)
+
+    def __call__(self, x: torch.Tensor, cross_attention: torch.Tensor | None = None) -> torch.Tensor:
+        """Apply multi-head attention.
+
+        Parameters
+        ----------
+        x
+            The input tensor.
+        cross_attention
+            The key and value tensors for cross-attention. If `None`, self-attention is applied.
+
+        Returns
+        -------
+            The output tensor.
+        """
+        return super().__call__(x, cross_attention)
+
+    def _reshape(self, x: torch.Tensor) -> torch.Tensor:
+        if not self.features_last:
+            x = x.moveaxis(1, -1)
+        return x.flatten(1, -2)
+
+    def forward(self, x: torch.Tensor, cross_attention: torch.Tensor | None = None) -> torch.Tensor:
+        """Apply multi-head attention."""
+        if cross_attention is None:
+            cross_attention = x
+        if not self.features_last:
+            x = x.moveaxis(1, -1)
+            cross_attention = cross_attention.moveaxis(1, -1)
+
+        query = rearrange(self.to_q(x), 'batch ... (heads channels) -> batch heads ... channels ', heads=self.n_heads)
+        key, value = rearrange(
+            self.to_kv(cross_attention),
+            'batch ... (kv heads channels) -> kv batch heads ... channels ',
+            heads=self.n_heads,
+            kv=2,
+        )
+        query, key = self.rope(query, key)  # NO-OP if rope_embed_fraction is 0.0
+        query, key, value = query.flatten(2, -2), key.flatten(2, -2), value.flatten(2, -2)
+        y = torch.nn.functional.scaled_dot_product_attention(
+            query, key, value, dropout_p=self.p_dropout, is_causal=False
+        )
+        y = rearrange(y, '... heads L channels -> ... L (heads channels)')
+        out = self.to_out(y).reshape(x.shape)
+
+        if not self.features_last:
+            out = out.moveaxis(-1, 1)
+
+        return out

src/mr2/nn/attention/SqueezeExcitation.py
@@ -1,0 +1,57 @@
+"""Squeeze-and-Excitation block."""
+
+import torch
+from torch.nn import Module, ReLU, Sigmoid
+
+from mr2.nn.ndmodules import adaptiveAvgPoolND, convND
+from mr2.nn.Sequential import Sequential
+
+
+class SqueezeExcitation(Module):
+    """Squeeze-and-Excitation block.
+
+    Sequeeze-and-Excitation block from [SE]_.
+
+    References
+    ----------
+    ..[SE] Hu, Jie, Li Shen, and Gang Sun. "Squeeze-and-excitation networks." CVPR 2018, https://arxiv.org/abs/1709.01507
+    """
+
+    def __init__(self, n_dim: int, n_channels_input: int, n_channels_squeeze: int) -> None:
+        """Initialize SqueezeExcitation.
+
+        Parameters
+        ----------
+        n_dim
+            The dimension of the input tensor.
+        n_channels_input
+            The number of channels in the input tensor.
+        n_channels_squeeze
+            The number of channels in the squeeze tensor.
+        """
+        super().__init__()
+        self.scale = Sequential(
+            adaptiveAvgPoolND(n_dim)(1),
+            convND(n_dim)(n_channels_input, n_channels_squeeze, kernel_size=1),
+            ReLU(),
+            convND(n_dim)(n_channels_squeeze, n_channels_input, kernel_size=1),
+            Sigmoid(),
+        )
+
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        """Apply SqueezeExcitation.
+
+        Parameters
+        ----------
+        x
+            The input tensor.
+
+        Returns
+        -------
+            The output tensor.
+        """
+        return super().__call__(x)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Apply SqueezeExcitation."""
+        return x * self.scale(x)

tests/nn/test_linearselfattention.py
@@ -1,0 +1,58 @@
+"""Tests for LinearSelfAttention module."""
+
+import pytest
+from mr2.nn.attention import LinearSelfAttention
+from mr2.utils import RandomGenerator
+
+
+@pytest.mark.parametrize(
+    'device',
+    [
+        pytest.param('cpu', id='cpu'),
+        pytest.param('cuda', id='cuda', marks=pytest.mark.cuda),
+    ],
+)
+@pytest.mark.parametrize(
+    ('n_channels_in', 'n_channels_out', 'n_heads', 'input_shape', 'features_last'),
+    [
+        (32, 32, 4, (1, 32, 32, 32), False),
+        (64, 64, 8, (2, 64, 16, 16), False),
+        (16, 16, 2, (1, 16, 16, 16), True),
+    ],
+)
+def test_linear_self_attention(
+    n_channels_in: int,
+    n_channels_out: int,
+    n_heads: int,
+    input_shape: tuple[int, ...],
+    features_last: bool,
+    device: str,
+) -> None:
+    """Test LinearSelfAttention output shape and backpropagation."""
+    rng = RandomGenerator(seed=42)
+    x = rng.float32_tensor(input_shape).to(device).requires_grad_(True)
+
+    attn = LinearSelfAttention(
+        n_channels_in=n_channels_in,
+        n_channels_out=n_channels_out,
+        n_heads=n_heads,
+        features_last=features_last,
+    ).to(device)
+
+    if features_last:
+        output = attn(x.moveaxis(1, -1)).moveaxis(-1, 1)
+    else:
+        output = attn(x)
+
+    expected_shape = (x.shape[0], n_channels_out, *x.shape[2:])
+    assert output.shape == expected_shape, f'Output shape {output.shape} != expected shape {expected_shape}'
+    assert not output.isnan().any(), 'NaN values in output'
+
+    output.sum().backward()
+    assert x.grad is not None, 'No gradient computed for input'
+    assert not x.grad.isnan().any(), 'NaN values in input gradients'
+
+    assert attn.to_qkv.weight.grad is not None, 'No gradient computed for to_qkv.weight'
+    assert attn.to_qkv.bias.grad is not None, 'No gradient computed for to_qkv.bias'
+    assert attn.to_out.weight.grad is not None, 'No gradient computed for to_out.weight'
+    assert attn.to_out.bias.grad is not None, 'No gradient computed for to_out.bias'

tests/nn/test_shiftedwindowattention.py
@@ -1,0 +1,61 @@
+"""Tests for ShiftedWindowAttention module."""
+
+import pytest
+from mr2.nn.attention import ShiftedWindowAttention
+from mr2.utils import RandomGenerator
+
+
+@pytest.mark.parametrize(
+    'device',
+    [
+        pytest.param('cpu', id='cpu'),
+        pytest.param('cuda', id='cuda', marks=pytest.mark.cuda),
+    ],
+)
+@pytest.mark.parametrize(
+    ('n_dim', 'window_size', 'shifted'),
+    [
+        (2, 8, False),
+        (4, 4, True),
+    ],
+)
+def test_shifted_window_attention(n_dim: int, window_size: int, shifted: bool, device: str) -> None:
+    """Test ShiftedWindowAttention output shape and backpropagation."""
+    n_batch, n_channels, n_heads = 2, 8, 2
+    spatial_shape = (window_size * 4,) * n_dim
+    rng = RandomGenerator(13)
+    x = rng.float32_tensor((n_batch, n_channels, *spatial_shape)).to(device).requires_grad_(True)
+    swin = ShiftedWindowAttention(
+        n_dim=n_dim,
+        n_channels_in=n_channels,
+        n_channels_out=n_channels,
+        n_heads=n_heads,
+        window_size=window_size,
+        shifted=shifted,
+    ).to(device)
+    out = swin(x)
+    assert out.shape == x.shape, f'Output shape {out.shape} != input shape {x.shape}'
+    assert not out.isnan().any(), 'NaN values in output'
+    out.sum().backward()
+    assert x.grad is not None, 'No gradient computed for input'
+    assert not x.grad.isnan().any(), 'NaN values in input gradients'
+    assert swin.to_qkv.weight.grad is not None, 'No gradient computed for to_qkv.weight'
+    assert swin.relative_position_bias_table.grad is not None, 'No gradient computed for relative_position_bias_table'
+
+
+@pytest.mark.parametrize('shifted', [True, False], ids=['shifted', 'non-shifted'])
+def test_shifted_window_attention_size_mismatch(shifted: bool):
+    n_batch, n_channels, n_heads, n_dim, window_size = 3, 4, 2, 2, 7
+    spatial_shape = (window_size * 4 + 1,) * n_dim
+    rng = RandomGenerator(13)
+    x = rng.float32_tensor((n_batch, n_channels, *spatial_shape))
+    swin = ShiftedWindowAttention(
+        n_dim=n_dim,
+        n_channels_in=n_channels,
+        n_channels_out=n_channels,
+        n_heads=n_heads,
+        window_size=window_size,
+        shifted=shifted,
+    )
+    out = swin(x)
+    assert out.shape == x.shape, f'Output shape {out.shape} != input shape {x.shape}'

tests/nn/test_squeezeexcitation.py
@@ -1,0 +1,32 @@
+"""Tests for SqueezeExcitation module."""
+
+from collections.abc import Sequence
+
+import pytest
+from mr2.nn.attention import SqueezeExcitation
+from mr2.utils import RandomGenerator
+
+
+@pytest.mark.parametrize(
+    ('dim', 'input_shape', 'squeeze_channels'),
+    [
+        (2, (1, 64, 32, 32), 16),
+        (3, (1, 64, 16, 16, 16), 16),
+    ],
+)
+def test_squeeze_excitation(
+    dim: int,
+    input_shape: Sequence[int],
+    squeeze_channels: int,
+) -> None:
+    """Test SqueezeExcitation output shape and backpropagation."""
+    rng = RandomGenerator(seed=42)
+    x = rng.float32_tensor(input_shape).requires_grad_(True)
+    se = SqueezeExcitation(n_dim=dim, n_channels_input=input_shape[1], n_channels_squeeze=squeeze_channels)
+    output = se(x)
+    assert output.shape == x.shape, f'Output shape {output.shape} != input shape {x.shape}'
+    output.sum().backward()
+    assert x.grad is not None, 'No gradient computed for input'
+    assert not output.isnan().any(), 'NaN values in output'
+    assert not x.grad.isnan().any(), 'NaN values in input gradients'
+    assert se.scale[1].weight.grad is not None, 'No gradient computed for Conv'