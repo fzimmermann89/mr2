PTB-MR/mrpro

add data consistency modules and tests
ghstack-source-id: 9eaa0e0f35023d9d5817c8d5edae5f1992271555
ghstack-comment-id: 3865650618
Pull-Request-resolved: https://github.com/PTB-MR/mrpro/pull/954

src/mrpro/nn/data_consistency/AnalyticCartesianDC.py
@@ -1,0 +1,101 @@
+"""Analytic Cartesian data consistency."""
+
+from typing import overload
+
+import torch
+from torch.nn import Module, Parameter
+
+from mrpro.data.KData import KData
+from mrpro.operators.FourierOp import FourierOp
+from mrpro.operators.IdentityOp import IdentityOp
+
+
+class AnalyticCartesianDC(Module):
+    r"""Analytic Cartesian data consistency.
+
+    Solves the following problem:
+    :math:`\min_x \|Ax - k\|_2^2 + \lambda \|x-p\|_2^2`
+    where :math:`A` is the acquisition operator and :math:`k` is the data, :math:`\lambda` is the regularization
+    parameter and :math:`p` is the regularization image/prior analytically. :math:`A^H A` has to be diagonal. This is a
+    special case for a Cartesian acquisition without coil sensitivity weighting. This can be used for either single-coil
+    data or to apply data consistency to each coil image [NOSENSE]_.
+
+    References
+    ----------
+    .. [NOSENSE] Zimmermann, FF, and Kofler, Andreas. "NoSENSE: Learned unrolled cardiac MRI reconstruction without
+        explicit sensitivity maps." STACOM@MICCAI 2023. https://arxiv.org/abs/2309.15608
+
+    Parameters
+    ----------
+    initial_regularization_weight
+        Initial regularization weight. The regularization weight is a trainable parameter.
+
+
+    """
+
+    def __init__(self, initial_regularization_weight: torch.Tensor | float):
+        """Initialize the data consistency.
+
+        Parameters
+        ----------
+        initial_regularization_weight
+            Initial regularization weight. The regularization weight is a trainable parameter.
+            Must be a positive scalar.
+        """
+        super().__init__()
+        weight = torch.as_tensor(initial_regularization_weight)
+        if weight.ndim != 0:
+            raise ValueError('Regularization weight must be a scalar')
+        if weight.item() <= 0:
+            raise ValueError('Regularization weight must be positive')
+        self.log_weight = Parameter(weight.log())
+
+    @overload
+    def __call__(self, image: torch.Tensor, data: KData, fourier_op: FourierOp | None = None) -> torch.Tensor: ...
+
+    @overload
+    def __call__(self, image: torch.Tensor, data: torch.Tensor, fourier_op: FourierOp) -> torch.Tensor: ...
+
+    def __call__(
+        self, image: torch.Tensor, data: KData | torch.Tensor, fourier_op: FourierOp | None = None
+    ) -> torch.Tensor:
+        """Apply the data consistency.
+
+        Parameters
+        ----------
+        image
+            Current image estimate, i.e. the regularized image.
+        data
+            k-space data.
+        fourier_op
+            Fourier operator matching the k-space data. If None and data is provided as a `~mrpro.data.KData` object,
+            the Fourier operator is automatically created from the data.
+
+        Returns
+        -------
+            Updated image estimate.
+        """
+        return super().__call__(image, data, fourier_op)
+
+    def forward(
+        self, image: torch.Tensor, data: KData | torch.Tensor, fourier_op: FourierOp | None = None
+    ) -> torch.Tensor:
+        """Apply the data consistency."""
+        if fourier_op is None:
+            if isinstance(data, KData):
+                fourier_op = FourierOp.from_kdata(data)
+            else:
+                raise ValueError('Either a KData or a FourierOp is required')
+
+        if not isinstance(fourier_op, FourierOp) or fourier_op._nufft_dims or fourier_op._fast_fourier_op is None:
+            raise ValueError('Only Cartesian acquisitions are supported')
+
+        data_ = data.data if isinstance(data, KData) else data
+        fft_op = fourier_op._fast_fourier_op
+        sampling_op = fourier_op._cart_sampling_op if fourier_op._cart_sampling_op is not None else IdentityOp()
+        (zero_filled,) = sampling_op.adjoint(data_)
+        (k_pred,) = fft_op(image)
+        regularization_weight = self.log_weight.exp()
+        (k,) = sampling_op.gram((zero_filled - k_pred) / (1 + regularization_weight))
+        (delta,) = fft_op.H(k)
+        return image + delta

src/mrpro/nn/data_consistency/GradientDescentDC.py
@@ -1,0 +1,99 @@
+"""Gradient descent data consistency."""
+
+from typing import overload
+
+import torch
+from torch.nn import Module, Parameter
+
+from mrpro.data.KData import KData
+from mrpro.operators.FourierOp import FourierOp
+from mrpro.operators.LinearOperator import LinearOperator
+
+
+class GradientDescentDC(Module):
+    r"""Gradient descent data consistency.
+
+    Performs gradient descent steps on
+    :math:`\|Ax - k\|_2^2` where :math:`A` is the acquisition operator and :math:`k` is the data.
+
+    Parameters
+    ----------
+    initial_stepsize
+        Initial stepsize. The stepsize is a trainable parameter.
+        Must be a positive scalar.
+    n_steps
+        Number of gradient descent steps.
+
+    Returns
+    -------
+        The updated image.
+    """
+
+    def __init__(self, initial_stepsize: float | torch.Tensor, n_steps: int = 1) -> None:
+        """Initialize the gradient descent data consistency.
+
+        Parameters
+        ----------
+        initial_stepsize
+            Initial stepsize. The stepsize is a trainable parameter.
+            Must be a positive scalar.
+        n_steps
+            Number of gradient descent steps.
+        """
+        super().__init__()
+        stepsize = torch.as_tensor(initial_stepsize)
+        if stepsize.ndim != 0:
+            raise ValueError('Stepsize must be a scalar')
+        if stepsize.item() <= 0:
+            raise ValueError('Stepsize must be positive')
+        self.log_stepsize = Parameter(stepsize.log())
+        self.n_steps = n_steps
+
+    @overload
+    def __call__(
+        self, image: torch.Tensor, data: KData, acquisition_operator: LinearOperator | None = None
+    ) -> torch.Tensor: ...
+
+    @overload
+    def __call__(
+        self, image: torch.Tensor, data: torch.Tensor, acquisition_operator: LinearOperator
+    ) -> torch.Tensor: ...
+
+    def __call__(
+        self, image: torch.Tensor, data: KData | torch.Tensor, acquisition_operator: LinearOperator | None = None
+    ) -> torch.Tensor:
+        """Apply the data consistency.
+
+        Parameters
+        ----------
+        image
+            Current image estimate.
+        data
+            k-space data.
+        acquisition_operator
+            Acquisition operator matching the k-space data. If None and data is provided as a `~mrpro.data.KData`
+            object, the Fourier operator is automatically created from the data.
+
+        Returns
+        -------
+            Updated image estimate.
+        """
+        return super().__call__(image, data, acquisition_operator)
+
+    def forward(
+        self, image: torch.Tensor, data: KData | torch.Tensor, acquisition_operator: LinearOperator | None = None
+    ) -> torch.Tensor:
+        """Apply the data consistency."""
+        if acquisition_operator is None:
+            if isinstance(data, KData):
+                acquisition_operator = FourierOp.from_kdata(data)
+            else:
+                raise ValueError('Either a KData or an acquisition operator is required')
+
+        data_ = data.data if isinstance(data, KData) else data
+        stepsize = self.log_stepsize.exp()
+        x = image
+        for _ in range(self.n_steps):
+            residual = acquisition_operator(x)[0] - data_
+            x = x - stepsize * acquisition_operator.adjoint(residual)[0]
+        return x

tests/nn/data_consistency/conftest.py
@@ -1,0 +1,46 @@
+"""Test fixtures for data consistency tests."""
+
+import pytest
+from mrpro.data.KData import KData
+from mrpro.data.SpatialDimension import SpatialDimension
+from mrpro.data.traj_calculators.KTrajectoryCartesian import KTrajectoryCartesian
+from mrpro.operators.FourierOp import FourierOp
+from mrpro.phantoms.EllipsePhantom import EllipsePhantom
+from mrpro.utils import RandomGenerator
+
+
+@pytest.fixture
+def kdata():
+    matrix = SpatialDimension(x=128, y=128, z=1)
+    kdata = EllipsePhantom().kdata(KTrajectoryCartesian.fullysampled(matrix), matrix)
+    return kdata
+
+
+@pytest.fixture
+def kdata_noisy(kdata: KData):
+    kdata_noisy = kdata.clone()
+    kdata_noisy.data += 0.1 * RandomGenerator(123).randn_like(kdata_noisy.data)
+    return kdata_noisy
+
+
+@pytest.fixture
+def kdata_us(kdata: KData):
+    return kdata[..., ::2, :].clone()
+
+
+@pytest.fixture
+def image_noisy(kdata_noisy: KData):
+    fourier_op = FourierOp.from_kdata(kdata_noisy)
+    return fourier_op.adjoint(kdata_noisy.data)[0]
+
+
+@pytest.fixture
+def image(kdata: KData):
+    fourier_op = FourierOp.from_kdata(kdata)
+    return fourier_op.adjoint(kdata.data)[0]
+
+
+@pytest.fixture
+def image_us(kdata_us: KData):
+    fourier_op = FourierOp.from_kdata(kdata_us)
+    return fourier_op.adjoint(kdata_us.data)[0]

tests/nn/data_consistency/test_analyticcartesiandc.py
@@ -1,0 +1,21 @@
+"""Tests for AnalyticCartesianDC module."""
+
+import torch
+from mrpro.data.KData import KData
+from mrpro.nn.data_consistency.AnalyticCartesianDC import AnalyticCartesianDC
+
+
+def test_analytic_cartesian_dc(
+    image_noisy: torch.Tensor, kdata_us: KData, image: torch.Tensor, image_us: torch.Tensor
+) -> None:
+    image_noisy = image_noisy.clone().requires_grad_(True)
+    dc = AnalyticCartesianDC(initial_regularization_weight=1e-6)
+    result = dc(image_noisy, kdata_us)
+    loss = (result - image).abs().mean()
+    assert loss < (image_noisy - image).abs().mean()
+    assert loss < (image_us - image).abs().mean()
+    loss.backward()
+    assert image_noisy.grad is not None
+    assert dc.log_weight.grad is not None
+    assert not dc.log_weight.grad.isnan()
+    assert not image_noisy.grad.isnan().any()

tests/nn/data_consistency/test_conjugategradientdc.py
@@ -1,0 +1,21 @@
+"""Tests for ConjugateGradientDC module."""
+
+import torch
+from mrpro.data.KData import KData
+from mrpro.nn.data_consistency.ConjugateGradientDC import ConjugateGradientDC
+
+
+def test_conjugate_gradient_dc(
+    image_noisy: torch.Tensor, kdata_us: KData, image: torch.Tensor, image_us: torch.Tensor
+) -> None:
+    image_noisy = image_noisy.clone().requires_grad_(True)
+    dc = ConjugateGradientDC(initial_regularization_weight=1.0)
+    result = dc(image_noisy, kdata_us)
+    loss = (result - image).abs().mean()
+    assert loss < (image_noisy - image).abs().mean()
+    assert loss < (image_us - image).abs().mean()
+    loss.backward()
+    assert image_noisy.grad is not None
+    assert dc.log_weight.grad is not None
+    assert not dc.log_weight.grad.isnan()
+    assert not image_noisy.grad.isnan().any()

tests/nn/data_consistency/test_gradientdescentdc.py
@@ -1,0 +1,21 @@
+"""Tests for GradientDescentDC module."""
+
+import torch
+from mrpro.data.KData import KData
+from mrpro.nn.data_consistency.GradientDescentDC import GradientDescentDC
+
+
+def test_gradient_descent_dc(
+    image_noisy: torch.Tensor, kdata_us: KData, image: torch.Tensor, image_us: torch.Tensor
+) -> None:
+    image_noisy = image_noisy.clone().requires_grad_(True)
+    dc = GradientDescentDC(initial_stepsize=1.0)
+    result = dc(image_noisy, kdata_us)
+    loss = (result - image).abs().mean()
+    assert loss < (image_noisy - image).abs().mean()
+    assert loss < (image_us - image).abs().mean()
+    loss.backward()
+    assert image_noisy.grad is not None
+    assert dc.log_stepsize.grad is not None
+    assert not dc.log_stepsize.grad.isnan()
+    assert not image_noisy.grad.isnan().any()

fzimmermann89/mr2

src/mr2/nn/data_consistency/AnalyticCartesianDC.py
@@ -1,0 +1,101 @@
+"""Analytic Cartesian data consistency."""
+
+from typing import overload
+
+import torch
+from torch.nn import Module, Parameter
+
+from mr2.data.KData import KData
+from mr2.operators.FourierOp import FourierOp
+from mr2.operators.IdentityOp import IdentityOp
+
+
+class AnalyticCartesianDC(Module):
+    r"""Analytic Cartesian data consistency.
+
+    Solves the following problem:
+    :math:`\min_x \|Ax - k\|_2^2 + \lambda \|x-p\|_2^2`
+    where :math:`A` is the acquisition operator and :math:`k` is the data, :math:`\lambda` is the regularization
+    parameter and :math:`p` is the regularization image/prior analytically. :math:`A^H A` has to be diagonal. This is a
+    special case for a Cartesian acquisition without coil sensitivity weighting. This can be used for either single-coil
+    data or to apply data consistency to each coil image [NOSENSE]_.
+
+    References
+    ----------
+    .. [NOSENSE] Zimmermann, FF, and Kofler, Andreas. "NoSENSE: Learned unrolled cardiac MRI reconstruction without
+        explicit sensitivity maps." STACOM@MICCAI 2023. https://arxiv.org/abs/2309.15608
+
+    Parameters
+    ----------
+    initial_regularization_weight
+        Initial regularization weight. The regularization weight is a trainable parameter.
+
+
+    """
+
+    def __init__(self, initial_regularization_weight: torch.Tensor | float):
+        """Initialize the data consistency.
+
+        Parameters
+        ----------
+        initial_regularization_weight
+            Initial regularization weight. The regularization weight is a trainable parameter.
+            Must be a positive scalar.
+        """
+        super().__init__()
+        weight = torch.as_tensor(initial_regularization_weight)
+        if weight.ndim != 0:
+            raise ValueError('Regularization weight must be a scalar')
+        if weight.item() <= 0:
+            raise ValueError('Regularization weight must be positive')
+        self.log_weight = Parameter(weight.log())
+
+    @overload
+    def __call__(self, image: torch.Tensor, data: KData, fourier_op: FourierOp | None = None) -> torch.Tensor: ...
+
+    @overload
+    def __call__(self, image: torch.Tensor, data: torch.Tensor, fourier_op: FourierOp) -> torch.Tensor: ...
+
+    def __call__(
+        self, image: torch.Tensor, data: KData | torch.Tensor, fourier_op: FourierOp | None = None
+    ) -> torch.Tensor:
+        """Apply the data consistency.
+
+        Parameters
+        ----------
+        image
+            Current image estimate, i.e. the regularized image.
+        data
+            k-space data.
+        fourier_op
+            Fourier operator matching the k-space data. If None and data is provided as a `~mr2.data.KData` object,
+            the Fourier operator is automatically created from the data.
+
+        Returns
+        -------
+            Updated image estimate.
+        """
+        return super().__call__(image, data, fourier_op)
+
+    def forward(
+        self, image: torch.Tensor, data: KData | torch.Tensor, fourier_op: FourierOp | None = None
+    ) -> torch.Tensor:
+        """Apply the data consistency."""
+        if fourier_op is None:
+            if isinstance(data, KData):
+                fourier_op = FourierOp.from_kdata(data)
+            else:
+                raise ValueError('Either a KData or a FourierOp is required')
+
+        if not isinstance(fourier_op, FourierOp) or fourier_op._nufft_dims or fourier_op._fast_fourier_op is None:
+            raise ValueError('Only Cartesian acquisitions are supported')
+
+        data_ = data.data if isinstance(data, KData) else data
+        fft_op = fourier_op._fast_fourier_op
+        sampling_op = fourier_op._cart_sampling_op if fourier_op._cart_sampling_op is not None else IdentityOp()
+        (zero_filled,) = sampling_op.adjoint(data_)
+        (k_pred,) = fft_op(image)
+        regularization_weight = self.log_weight.exp()
+        (k,) = sampling_op.gram((zero_filled - k_pred) / (1 + regularization_weight))
+        (delta,) = fft_op.H(k)
+        return image + delta

src/mr2/nn/data_consistency/GradientDescentDC.py
@@ -1,0 +1,99 @@
+"""Gradient descent data consistency."""
+
+from typing import overload
+
+import torch
+from torch.nn import Module, Parameter
+
+from mr2.data.KData import KData
+from mr2.operators.FourierOp import FourierOp
+from mr2.operators.LinearOperator import LinearOperator
+
+
+class GradientDescentDC(Module):
+    r"""Gradient descent data consistency.
+
+    Performs gradient descent steps on
+    :math:`\|Ax - k\|_2^2` where :math:`A` is the acquisition operator and :math:`k` is the data.
+
+    Parameters
+    ----------
+    initial_stepsize
+        Initial stepsize. The stepsize is a trainable parameter.
+        Must be a positive scalar.
+    n_steps
+        Number of gradient descent steps.
+
+    Returns
+    -------
+        The updated image.
+    """
+
+    def __init__(self, initial_stepsize: float | torch.Tensor, n_steps: int = 1) -> None:
+        """Initialize the gradient descent data consistency.
+
+        Parameters
+        ----------
+        initial_stepsize
+            Initial stepsize. The stepsize is a trainable parameter.
+            Must be a positive scalar.
+        n_steps
+            Number of gradient descent steps.
+        """
+        super().__init__()
+        stepsize = torch.as_tensor(initial_stepsize)
+        if stepsize.ndim != 0:
+            raise ValueError('Stepsize must be a scalar')
+        if stepsize.item() <= 0:
+            raise ValueError('Stepsize must be positive')
+        self.log_stepsize = Parameter(stepsize.log())
+        self.n_steps = n_steps
+
+    @overload
+    def __call__(
+        self, image: torch.Tensor, data: KData, acquisition_operator: LinearOperator | None = None
+    ) -> torch.Tensor: ...
+
+    @overload
+    def __call__(
+        self, image: torch.Tensor, data: torch.Tensor, acquisition_operator: LinearOperator
+    ) -> torch.Tensor: ...
+
+    def __call__(
+        self, image: torch.Tensor, data: KData | torch.Tensor, acquisition_operator: LinearOperator | None = None
+    ) -> torch.Tensor:
+        """Apply the data consistency.
+
+        Parameters
+        ----------
+        image
+            Current image estimate.
+        data
+            k-space data.
+        acquisition_operator
+            Acquisition operator matching the k-space data. If None and data is provided as a `~mr2.data.KData`
+            object, the Fourier operator is automatically created from the data.
+
+        Returns
+        -------
+            Updated image estimate.
+        """
+        return super().__call__(image, data, acquisition_operator)
+
+    def forward(
+        self, image: torch.Tensor, data: KData | torch.Tensor, acquisition_operator: LinearOperator | None = None
+    ) -> torch.Tensor:
+        """Apply the data consistency."""
+        if acquisition_operator is None:
+            if isinstance(data, KData):
+                acquisition_operator = FourierOp.from_kdata(data)
+            else:
+                raise ValueError('Either a KData or an acquisition operator is required')
+
+        data_ = data.data if isinstance(data, KData) else data
+        stepsize = self.log_stepsize.exp()
+        x = image
+        for _ in range(self.n_steps):
+            residual = acquisition_operator(x)[0] - data_
+            x = x - stepsize * acquisition_operator.adjoint(residual)[0]
+        return x

tests/nn/data_consistency/conftest.py
@@ -1,0 +1,46 @@
+"""Test fixtures for data consistency tests."""
+
+import pytest
+from mr2.data.KData import KData
+from mr2.data.SpatialDimension import SpatialDimension
+from mr2.data.traj_calculators.KTrajectoryCartesian import KTrajectoryCartesian
+from mr2.operators.FourierOp import FourierOp
+from mr2.phantoms.EllipsePhantom import EllipsePhantom
+from mr2.utils import RandomGenerator
+
+
+@pytest.fixture
+def kdata():
+    matrix = SpatialDimension(x=128, y=128, z=1)
+    kdata = EllipsePhantom().kdata(KTrajectoryCartesian.fullysampled(matrix), matrix)
+    return kdata
+
+
+@pytest.fixture
+def kdata_noisy(kdata: KData):
+    kdata_noisy = kdata.clone()
+    kdata_noisy.data += 0.1 * RandomGenerator(123).randn_like(kdata_noisy.data)
+    return kdata_noisy
+
+
+@pytest.fixture
+def kdata_us(kdata: KData):
+    return kdata[..., ::2, :].clone()
+
+
+@pytest.fixture
+def image_noisy(kdata_noisy: KData):
+    fourier_op = FourierOp.from_kdata(kdata_noisy)
+    return fourier_op.adjoint(kdata_noisy.data)[0]
+
+
+@pytest.fixture
+def image(kdata: KData):
+    fourier_op = FourierOp.from_kdata(kdata)
+    return fourier_op.adjoint(kdata.data)[0]
+
+
+@pytest.fixture
+def image_us(kdata_us: KData):
+    fourier_op = FourierOp.from_kdata(kdata_us)
+    return fourier_op.adjoint(kdata_us.data)[0]

tests/nn/data_consistency/test_analyticcartesiandc.py
@@ -1,0 +1,21 @@
+"""Tests for AnalyticCartesianDC module."""
+
+import torch
+from mr2.data.KData import KData
+from mr2.nn.data_consistency.AnalyticCartesianDC import AnalyticCartesianDC
+
+
+def test_analytic_cartesian_dc(
+    image_noisy: torch.Tensor, kdata_us: KData, image: torch.Tensor, image_us: torch.Tensor
+) -> None:
+    image_noisy = image_noisy.clone().requires_grad_(True)
+    dc = AnalyticCartesianDC(initial_regularization_weight=1e-6)
+    result = dc(image_noisy, kdata_us)
+    loss = (result - image).abs().mean()
+    assert loss < (image_noisy - image).abs().mean()
+    assert loss < (image_us - image).abs().mean()
+    loss.backward()
+    assert image_noisy.grad is not None
+    assert dc.log_weight.grad is not None
+    assert not dc.log_weight.grad.isnan()
+    assert not image_noisy.grad.isnan().any()

tests/nn/data_consistency/test_conjugategradientdc.py
@@ -1,0 +1,21 @@
+"""Tests for ConjugateGradientDC module."""
+
+import torch
+from mr2.data.KData import KData
+from mr2.nn.data_consistency.ConjugateGradientDC import ConjugateGradientDC
+
+
+def test_conjugate_gradient_dc(
+    image_noisy: torch.Tensor, kdata_us: KData, image: torch.Tensor, image_us: torch.Tensor
+) -> None:
+    image_noisy = image_noisy.clone().requires_grad_(True)
+    dc = ConjugateGradientDC(initial_regularization_weight=1.0)
+    result = dc(image_noisy, kdata_us)
+    loss = (result - image).abs().mean()
+    assert loss < (image_noisy - image).abs().mean()
+    assert loss < (image_us - image).abs().mean()
+    loss.backward()
+    assert image_noisy.grad is not None
+    assert dc.log_weight.grad is not None
+    assert not dc.log_weight.grad.isnan()
+    assert not image_noisy.grad.isnan().any()

tests/nn/data_consistency/test_gradientdescentdc.py
@@ -1,0 +1,21 @@
+"""Tests for GradientDescentDC module."""
+
+import torch
+from mr2.data.KData import KData
+from mr2.nn.data_consistency.GradientDescentDC import GradientDescentDC
+
+
+def test_gradient_descent_dc(
+    image_noisy: torch.Tensor, kdata_us: KData, image: torch.Tensor, image_us: torch.Tensor
+) -> None:
+    image_noisy = image_noisy.clone().requires_grad_(True)
+    dc = GradientDescentDC(initial_stepsize=1.0)
+    result = dc(image_noisy, kdata_us)
+    loss = (result - image).abs().mean()
+    assert loss < (image_noisy - image).abs().mean()
+    assert loss < (image_us - image).abs().mean()
+    loss.backward()
+    assert image_noisy.grad is not None
+    assert dc.log_stepsize.grad is not None
+    assert not dc.log_stepsize.grad.isnan()
+    assert not image_noisy.grad.isnan().any()