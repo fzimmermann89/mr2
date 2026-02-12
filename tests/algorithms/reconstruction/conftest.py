PTB-MR/mrpro

Add tests for reconstructions (#967)

tests/algorithms/reconstruction/conftest.py
@@ -1,0 +1,30 @@
+"""PyTest fixtures for reconstruction tests."""
+
+from collections.abc import Callable
+
+import pytest
+from mrpro.algorithms.reconstruction import DirectReconstruction
+from mrpro.data import CsmData, DcfData, KData
+from mrpro.data.traj_calculators import KTrajectoryCartesian
+from mrpro.operators import FourierOp
+from tests.conftest import ismrmrd_cart as _ismrmrd_cart  # noqa: F401
+from tests.data import IsmrmrdRawTestData
+
+
+@pytest.fixture(scope='session')
+def cartesian_kdata(ismrmrd_cart: IsmrmrdRawTestData) -> KData:
+    """Create Cartesian KData from shared ISMRMRD test fixture."""
+    return KData.from_file(ismrmrd_cart.filename, KTrajectoryCartesian())
+
+
+@pytest.fixture
+def explicit_components() -> Callable[[KData], tuple[FourierOp, CsmData, DcfData]]:
+    """Create explicit FourierOp, CSM, and DCF from KData."""
+
+    def _explicit_components(kdata: KData) -> tuple[FourierOp, CsmData, DcfData]:
+        direct_reconstruction = DirectReconstruction(kdata=kdata, csm=None)
+        csm = CsmData.from_idata_walsh(direct_reconstruction(kdata))
+        assert direct_reconstruction.dcf is not None
+        return FourierOp.from_kdata(kdata), csm, direct_reconstruction.dcf
+
+    return _explicit_components

tests/algorithms/reconstruction/test_directreconstruction.py
@@ -1,0 +1,88 @@
+"""Tests for DirectReconstruction."""
+
+from collections.abc import Callable
+
+import pytest
+from mrpro.algorithms.reconstruction import DirectReconstruction
+from mrpro.data import CsmData, DcfData, KData
+from mrpro.operators import FourierOp
+
+
+def test_direct_reconstruction_automatic(cartesian_kdata: KData) -> None:
+    """Test automatic setup from kdata only."""
+    reconstruction = DirectReconstruction(kdata=cartesian_kdata)
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert reconstruction.csm is not None
+    assert reconstruction.dcf is not None
+
+
+def test_direct_reconstruction_with_explicit_csm(cartesian_kdata: KData) -> None:
+    """Test with pre-computed CSM."""
+    csm = CsmData.from_idata_walsh(DirectReconstruction(kdata=cartesian_kdata)(cartesian_kdata))
+    reconstruction = DirectReconstruction(kdata=cartesian_kdata, csm=csm)
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert reconstruction.csm is csm
+
+
+def test_direct_reconstruction_with_explicit_dcf(cartesian_kdata: KData) -> None:
+    """Test with pre-computed DCF."""
+    dcf = DirectReconstruction(kdata=cartesian_kdata).dcf
+    reconstruction = DirectReconstruction(kdata=cartesian_kdata, dcf=dcf)
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert reconstruction.dcf is dcf
+
+
+def test_direct_reconstruction_with_explicit_fourier_op(cartesian_kdata: KData) -> None:
+    """Test with pre-computed FourierOp."""
+    fourier_op = FourierOp.from_kdata(cartesian_kdata)
+    reconstruction = DirectReconstruction(kdata=cartesian_kdata, fourier_op=fourier_op)
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert reconstruction.fourier_op is fourier_op
+
+
+@pytest.mark.cuda
+def test_direct_reconstruction_cuda_from_kdata(cartesian_kdata: KData) -> None:
+    """Test CUDA device transfers for reconstruction created from kdata."""
+    reconstruction = DirectReconstruction(kdata=cartesian_kdata).cuda()
+    idata = reconstruction(cartesian_kdata.cuda())
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert idata.is_cuda
+
+    reconstruction = DirectReconstruction(kdata=cartesian_kdata.cuda())
+    idata = reconstruction(cartesian_kdata.cuda())
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert idata.is_cuda
+
+    reconstruction = DirectReconstruction(kdata=cartesian_kdata.cuda()).cpu()
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert idata.is_cpu
+
+
+@pytest.mark.cuda
+def test_direct_reconstruction_cuda_explicit_components(
+    cartesian_kdata: KData,
+    explicit_components: Callable[[KData], tuple[FourierOp, CsmData, DcfData]],
+) -> None:
+    """Test CUDA device transfers with explicit FourierOp, CSM, and DCF."""
+    fourier_op, csm, dcf = explicit_components(cartesian_kdata)
+    reconstruction = DirectReconstruction(fourier_op=fourier_op, csm=csm, dcf=dcf).cuda()
+    idata = reconstruction(cartesian_kdata.cuda())
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert idata.is_cuda
+
+    fourier_op, csm, dcf = explicit_components(cartesian_kdata.cuda())
+    reconstruction = DirectReconstruction(fourier_op=fourier_op, csm=csm, dcf=dcf)
+    idata = reconstruction(cartesian_kdata.cuda())
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert idata.is_cuda
+
+    fourier_op, csm, dcf = explicit_components(cartesian_kdata.cuda())
+    reconstruction = DirectReconstruction(fourier_op=fourier_op, csm=csm, dcf=dcf).cpu()
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert idata.is_cpu

tests/algorithms/reconstruction/test_iterativesense.py
@@ -1,0 +1,106 @@
+"""Tests for IterativeSENSEReconstruction."""
+
+from collections.abc import Callable
+
+import pytest
+from mrpro.algorithms.reconstruction import IterativeSENSEReconstruction, RegularizedIterativeSENSEReconstruction
+from mrpro.data import CsmData, DcfData, KData
+from mrpro.operators import FourierOp
+
+
+def test_iterative_sense_automatic(cartesian_kdata: KData) -> None:
+    """Test automatic setup from kdata only."""
+    reconstruction = IterativeSENSEReconstruction(kdata=cartesian_kdata, n_iterations=2)
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert reconstruction.csm is not None
+    assert reconstruction.dcf is not None
+
+
+def test_iterative_sense_with_callable_csm(cartesian_kdata: KData) -> None:
+    """Test with callable CSM estimation."""
+    reconstruction = IterativeSENSEReconstruction(
+        kdata=cartesian_kdata,
+        csm=CsmData.from_idata_walsh,
+        n_iterations=2,
+    )
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert reconstruction.csm is not None
+
+
+def test_regularized_iterative_sense_with_callable_csm(cartesian_kdata: KData) -> None:
+    """Test regularized iterative SENSE with callable CSM estimation."""
+    reconstruction = RegularizedIterativeSENSEReconstruction(
+        kdata=cartesian_kdata,
+        csm=CsmData.from_idata_walsh,
+        n_iterations=2,
+        regularization_weight=0.01,
+    )
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert reconstruction.csm is not None
+
+
+def test_iterative_sense_with_explicit_csm(cartesian_kdata: KData) -> None:
+    """Test with pre-computed CSM."""
+    csm = CsmData.from_idata_walsh(
+        IterativeSENSEReconstruction(kdata=cartesian_kdata, n_iterations=2)(cartesian_kdata),
+    )
+    reconstruction = IterativeSENSEReconstruction(kdata=cartesian_kdata, csm=csm, n_iterations=2)
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert reconstruction.csm is csm
+
+
+def test_iterative_sense_with_explicit_dcf(cartesian_kdata: KData) -> None:
+    """Test with pre-computed DCF."""
+    dcf = IterativeSENSEReconstruction(kdata=cartesian_kdata, n_iterations=2).dcf
+    reconstruction = IterativeSENSEReconstruction(kdata=cartesian_kdata, dcf=dcf, n_iterations=2)
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert reconstruction.dcf is dcf
+
+
+@pytest.mark.cuda
+def test_iterative_sense_cuda_from_kdata(cartesian_kdata: KData) -> None:
+    """Test CUDA device transfers for reconstruction created from kdata."""
+    reconstruction = IterativeSENSEReconstruction(kdata=cartesian_kdata, n_iterations=2).cuda()
+    idata = reconstruction(cartesian_kdata.cuda())
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert idata.is_cuda
+
+    reconstruction = IterativeSENSEReconstruction(kdata=cartesian_kdata.cuda(), n_iterations=2)
+    idata = reconstruction(cartesian_kdata.cuda())
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert idata.is_cuda
+
+    reconstruction = IterativeSENSEReconstruction(kdata=cartesian_kdata.cuda(), n_iterations=2).cpu()
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert idata.is_cpu
+
+
+@pytest.mark.cuda
+def test_iterative_sense_cuda_explicit_components(
+    cartesian_kdata: KData,
+    explicit_components: Callable[[KData], tuple[FourierOp, CsmData, DcfData]],
+) -> None:
+    """Test CUDA device transfers with explicit FourierOp, CSM, and DCF."""
+    fourier_op, csm, dcf = explicit_components(cartesian_kdata)
+    reconstruction = IterativeSENSEReconstruction(fourier_op=fourier_op, csm=csm, dcf=dcf, n_iterations=2).cuda()
+    idata = reconstruction(cartesian_kdata.cuda())
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert idata.is_cuda
+
+    fourier_op, csm, dcf = explicit_components(cartesian_kdata.cuda())
+    reconstruction = IterativeSENSEReconstruction(fourier_op=fourier_op, csm=csm, dcf=dcf, n_iterations=2)
+    idata = reconstruction(cartesian_kdata.cuda())
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert idata.is_cuda
+
+    fourier_op, csm, dcf = explicit_components(cartesian_kdata.cuda())
+    reconstruction = IterativeSENSEReconstruction(fourier_op=fourier_op, csm=csm, dcf=dcf, n_iterations=2).cpu()
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert idata.is_cpu

fzimmermann89/mr2

tests/algorithms/reconstruction/conftest.py
@@ -1,0 +1,30 @@
+"""PyTest fixtures for reconstruction tests."""
+
+from collections.abc import Callable
+
+import pytest
+from mr2.algorithms.reconstruction import DirectReconstruction
+from mr2.data import CsmData, DcfData, KData
+from mr2.data.traj_calculators import KTrajectoryCartesian
+from mr2.operators import FourierOp
+from tests.conftest import ismrmrd_cart as _ismrmrd_cart  # noqa: F401
+from tests.data import IsmrmrdRawTestData
+
+
+@pytest.fixture(scope='session')
+def cartesian_kdata(ismrmrd_cart: IsmrmrdRawTestData) -> KData:
+    """Create Cartesian KData from shared ISMRMRD test fixture."""
+    return KData.from_file(ismrmrd_cart.filename, KTrajectoryCartesian())
+
+
+@pytest.fixture
+def explicit_components() -> Callable[[KData], tuple[FourierOp, CsmData, DcfData]]:
+    """Create explicit FourierOp, CSM, and DCF from KData."""
+
+    def _explicit_components(kdata: KData) -> tuple[FourierOp, CsmData, DcfData]:
+        direct_reconstruction = DirectReconstruction(kdata=kdata, csm=None)
+        csm = CsmData.from_idata_walsh(direct_reconstruction(kdata))
+        assert direct_reconstruction.dcf is not None
+        return FourierOp.from_kdata(kdata), csm, direct_reconstruction.dcf
+
+    return _explicit_components

tests/algorithms/reconstruction/test_directreconstruction.py
@@ -1,0 +1,90 @@
+"""Tests for DirectReconstruction."""
+
+from collections.abc import Callable
+
+import pytest
+from mr2.algorithms.reconstruction import DirectReconstruction
+from mr2.data import CsmData, DcfData, KData
+from mr2.operators import FourierOp
+
+
+def test_direct_reconstruction_automatic(cartesian_kdata: KData) -> None:
+    """Test automatic setup from kdata only."""
+    reconstruction = DirectReconstruction(kdata=cartesian_kdata)
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert reconstruction.csm is not None
+    assert reconstruction.dcf is not None
+
+
+def test_direct_reconstruction_with_explicit_csm(cartesian_kdata: KData) -> None:
+    """Test with pre-computed CSM."""
+    csm = CsmData.from_idata_walsh(DirectReconstruction(kdata=cartesian_kdata)(cartesian_kdata))
+    reconstruction = DirectReconstruction(kdata=cartesian_kdata, csm=csm)
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert reconstruction.csm is csm
+
+
+def test_direct_reconstruction_with_explicit_dcf(cartesian_kdata: KData) -> None:
+    """Test with pre-computed DCF."""
+    dcf = DirectReconstruction(kdata=cartesian_kdata).dcf
+    reconstruction = DirectReconstruction(kdata=cartesian_kdata, dcf=dcf)
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert reconstruction.dcf is dcf
+
+
+def test_direct_reconstruction_with_explicit_fourier_op(cartesian_kdata: KData) -> None:
+    """Test with pre-computed FourierOp."""
+    fourier_op = FourierOp.from_kdata(cartesian_kdata)
+    reconstruction = DirectReconstruction(kdata=cartesian_kdata, fourier_op=fourier_op)
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert reconstruction.fourier_op is fourier_op
+
+
+@pytest.mark.cuda
+@pytest.mark.xfail(reason='Known CUDA reconstruction failure', strict=False)
+def test_direct_reconstruction_cuda_from_kdata(cartesian_kdata: KData) -> None:
+    """Test CUDA device transfers for reconstruction created from kdata."""
+    reconstruction = DirectReconstruction(kdata=cartesian_kdata).cuda()
+    idata = reconstruction(cartesian_kdata.cuda())
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert idata.is_cuda
+
+    reconstruction = DirectReconstruction(kdata=cartesian_kdata.cuda())
+    idata = reconstruction(cartesian_kdata.cuda())
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert idata.is_cuda
+
+    reconstruction = DirectReconstruction(kdata=cartesian_kdata.cuda()).cpu()
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert idata.is_cpu
+
+
+@pytest.mark.cuda
+@pytest.mark.xfail(reason='Known CUDA reconstruction failure', strict=False)
+def test_direct_reconstruction_cuda_explicit_components(
+    cartesian_kdata: KData,
+    explicit_components: Callable[[KData], tuple[FourierOp, CsmData, DcfData]],
+) -> None:
+    """Test CUDA device transfers with explicit FourierOp, CSM, and DCF."""
+    fourier_op, csm, dcf = explicit_components(cartesian_kdata)
+    reconstruction = DirectReconstruction(fourier_op=fourier_op, csm=csm, dcf=dcf).cuda()
+    idata = reconstruction(cartesian_kdata.cuda())
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert idata.is_cuda
+
+    fourier_op, csm, dcf = explicit_components(cartesian_kdata.cuda())
+    reconstruction = DirectReconstruction(fourier_op=fourier_op, csm=csm, dcf=dcf)
+    idata = reconstruction(cartesian_kdata.cuda())
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert idata.is_cuda
+
+    fourier_op, csm, dcf = explicit_components(cartesian_kdata.cuda())
+    reconstruction = DirectReconstruction(fourier_op=fourier_op, csm=csm, dcf=dcf).cpu()
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert idata.is_cpu

tests/algorithms/reconstruction/test_iterativesense.py
@@ -1,0 +1,108 @@
+"""Tests for IterativeSENSEReconstruction."""
+
+from collections.abc import Callable
+
+import pytest
+from mr2.algorithms.reconstruction import IterativeSENSEReconstruction, RegularizedIterativeSENSEReconstruction
+from mr2.data import CsmData, DcfData, KData
+from mr2.operators import FourierOp
+
+
+def test_iterative_sense_automatic(cartesian_kdata: KData) -> None:
+    """Test automatic setup from kdata only."""
+    reconstruction = IterativeSENSEReconstruction(kdata=cartesian_kdata, n_iterations=2)
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert reconstruction.csm is not None
+    assert reconstruction.dcf is not None
+
+
+def test_iterative_sense_with_callable_csm(cartesian_kdata: KData) -> None:
+    """Test with callable CSM estimation."""
+    reconstruction = IterativeSENSEReconstruction(
+        kdata=cartesian_kdata,
+        csm=CsmData.from_idata_walsh,
+        n_iterations=2,
+    )
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert reconstruction.csm is not None
+
+
+def test_regularized_iterative_sense_with_callable_csm(cartesian_kdata: KData) -> None:
+    """Test regularized iterative SENSE with callable CSM estimation."""
+    reconstruction = RegularizedIterativeSENSEReconstruction(
+        kdata=cartesian_kdata,
+        csm=CsmData.from_idata_walsh,
+        n_iterations=2,
+        regularization_weight=0.01,
+    )
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert reconstruction.csm is not None
+
+
+def test_iterative_sense_with_explicit_csm(cartesian_kdata: KData) -> None:
+    """Test with pre-computed CSM."""
+    csm = CsmData.from_idata_walsh(
+        IterativeSENSEReconstruction(kdata=cartesian_kdata, n_iterations=2)(cartesian_kdata),
+    )
+    reconstruction = IterativeSENSEReconstruction(kdata=cartesian_kdata, csm=csm, n_iterations=2)
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert reconstruction.csm is csm
+
+
+def test_iterative_sense_with_explicit_dcf(cartesian_kdata: KData) -> None:
+    """Test with pre-computed DCF."""
+    dcf = IterativeSENSEReconstruction(kdata=cartesian_kdata, n_iterations=2).dcf
+    reconstruction = IterativeSENSEReconstruction(kdata=cartesian_kdata, dcf=dcf, n_iterations=2)
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert reconstruction.dcf is dcf
+
+
+@pytest.mark.cuda
+@pytest.mark.xfail(reason='Known CUDA reconstruction failure', strict=False)
+def test_iterative_sense_cuda_from_kdata(cartesian_kdata: KData) -> None:
+    """Test CUDA device transfers for reconstruction created from kdata."""
+    reconstruction = IterativeSENSEReconstruction(kdata=cartesian_kdata, n_iterations=2).cuda()
+    idata = reconstruction(cartesian_kdata.cuda())
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert idata.is_cuda
+
+    reconstruction = IterativeSENSEReconstruction(kdata=cartesian_kdata.cuda(), n_iterations=2)
+    idata = reconstruction(cartesian_kdata.cuda())
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert idata.is_cuda
+
+    reconstruction = IterativeSENSEReconstruction(kdata=cartesian_kdata.cuda(), n_iterations=2).cpu()
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert idata.is_cpu
+
+
+@pytest.mark.cuda
+@pytest.mark.xfail(reason='Known CUDA reconstruction failure', strict=False)
+def test_iterative_sense_cuda_explicit_components(
+    cartesian_kdata: KData,
+    explicit_components: Callable[[KData], tuple[FourierOp, CsmData, DcfData]],
+) -> None:
+    """Test CUDA device transfers with explicit FourierOp, CSM, and DCF."""
+    fourier_op, csm, dcf = explicit_components(cartesian_kdata)
+    reconstruction = IterativeSENSEReconstruction(fourier_op=fourier_op, csm=csm, dcf=dcf, n_iterations=2).cuda()
+    idata = reconstruction(cartesian_kdata.cuda())
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert idata.is_cuda
+
+    fourier_op, csm, dcf = explicit_components(cartesian_kdata.cuda())
+    reconstruction = IterativeSENSEReconstruction(fourier_op=fourier_op, csm=csm, dcf=dcf, n_iterations=2)
+    idata = reconstruction(cartesian_kdata.cuda())
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert idata.is_cuda
+
+    fourier_op, csm, dcf = explicit_components(cartesian_kdata.cuda())
+    reconstruction = IterativeSENSEReconstruction(fourier_op=fourier_op, csm=csm, dcf=dcf, n_iterations=2).cpu()
+    idata = reconstruction(cartesian_kdata)
+    assert idata.data.shape[-3:] == cartesian_kdata.header.recon_matrix.zyx
+    assert idata.is_cpu