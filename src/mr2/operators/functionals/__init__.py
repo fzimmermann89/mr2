64f2a3f3cddc387c60c14871d452dd6d14a1d4e6
Felix Zimmermann <fzimmermann89@gmail.com>
2026-03-01 14:35:47 +0100

    Add NCC functional

--- src/mr2/operators/functionals/__init__.py
+++ src/mr2/operators/functionals/__init__.py
@@ -2,6 +2,8 @@ from mr2.operators.functionals.L1Norm import L1Norm
 from mr2.operators.functionals.L1NormViewAsReal import L1NormViewAsReal
 from mr2.operators.functionals.L2NormSquared import L2NormSquared
 from mr2.operators.functionals.MSE import MSE
+from mr2.operators.functionals.NCC import NCC
 from mr2.operators.functionals.SSIM import SSIM
 from mr2.operators.functionals.ZeroFunctional import ZeroFunctional
-__all__ = ["L1Norm", "L1NormViewAsReal", "L2NormSquared", "MSE", "SSIM","ZeroFunctional"]
+
+__all__ = ['L1Norm', 'L1NormViewAsReal', 'L2NormSquared', 'MSE', 'NCC', 'SSIM', 'ZeroFunctional']
