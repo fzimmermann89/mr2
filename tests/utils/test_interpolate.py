Kelleretoro/mrpro

Add interp (#983)

tests/utils/test_interpolate.py
@@ -2,7 +2,7 @@
 
 import pytest
 import torch
-from mrpro.utils.interpolate import apply_lowres, interpolate
+from mrpro.utils.interpolate import apply_lowres, interp, interpolate
 
 
 @pytest.fixture
@@ -50,3 +50,23 @@ def test_apply_lowres(data: torch.Tensor) -> None:
 
     apply = apply_lowres(identity_function, size=(8, 8, 8), dim=(-3, -2, -1))
     torch.testing.assert_close(data[..., 1:-1], apply(data)[..., 1:-1])
+
+
+def test_interp() -> None:
+    """Test interp vs expected values"""
+    xp = torch.tensor([1.0, 2.0, 3.0])
+    fp = torch.tensor([10.0, 20.0, 40.0])
+    x = torch.tensor([[0.99, 1.0, 1.5], [2.5, 3.0, 3.01]])
+    expected = torch.tensor([[10.0, 10.0, 15.0], [30.0, 40.0, 40.0]])
+    result = interp(x, xp, fp)
+    torch.testing.assert_close(result, expected)
+
+
+def test_interp_unsorted_xp() -> None:
+    """Test interp with unsorted xp."""
+    xp = torch.tensor([3.0, 2.0, 1.0])
+    fp = torch.tensor([40.0, 20.0, 10.0])
+    x = torch.tensor([[0.99, 1.0, 1.5], [2.5, 3.0, 3.01]])
+    expected = torch.tensor([[10.0, 10.0, 15.0], [30.0, 40.0, 40.0]])
+    result = interp(x, xp, fp)
+    torch.testing.assert_close(result, expected)

fzimmermann89/mr2

(cherry picked from commit beeeb21b68020244c45210868a56a2d2502487a7)

tests/utils/test_interpolate.py
@@ -2,7 +2,7 @@
 
 import pytest
 import torch
-from mr2.utils.interpolate import apply_lowres, interpolate
+from mr2.utils.interpolate import apply_lowres, interp, interpolate
 
 
 @pytest.fixture
@@ -50,3 +50,23 @@ def test_apply_lowres(data: torch.Tensor) -> None:
 
     apply = apply_lowres(identity_function, size=(8, 8, 8), dim=(-3, -2, -1))
     torch.testing.assert_close(data[..., 1:-1], apply(data)[..., 1:-1])
+
+
+def test_interp() -> None:
+    """Test interp vs expected values"""
+    xp = torch.tensor([1.0, 2.0, 3.0])
+    fp = torch.tensor([10.0, 20.0, 40.0])
+    x = torch.tensor([[0.99, 1.0, 1.5], [2.5, 3.0, 3.01]])
+    expected = torch.tensor([[10.0, 10.0, 15.0], [30.0, 40.0, 40.0]])
+    result = interp(x, xp, fp)
+    torch.testing.assert_close(result, expected)
+
+
+def test_interp_unsorted_xp() -> None:
+    """Test interp with unsorted xp."""
+    xp = torch.tensor([3.0, 2.0, 1.0])
+    fp = torch.tensor([40.0, 20.0, 10.0])
+    x = torch.tensor([[0.99, 1.0, 1.5], [2.5, 3.0, 3.01]])
+    expected = torch.tensor([[10.0, 10.0, 15.0], [30.0, 40.0, 40.0]])
+    result = interp(x, xp, fp)
+    torch.testing.assert_close(result, expected)