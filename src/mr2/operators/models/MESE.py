Kelleretoro/mrpro

Add more EPG signal models (#973)

src/mrpro/operators/models/MESE.py
@@ -1,0 +1,110 @@
+"""Multi-echo spin echo (MESE) signal model (EPG)."""
+
+import torch
+
+from mrpro.operators.models.EPG import EPGSequence, Parameters, TseBlock
+from mrpro.operators.SignalModel import SignalModel
+
+
+class MultiEchoSpinEcho(SignalModel[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor | None]):
+    """Multi-echo spin echo (MESE) signal model.
+
+    This model simulates a multi-echo spin echo sequence using the extended
+    phase graph (`~mr2.operators.models.EPG`) formalism.
+
+    The sequence consists of an excitation pulse followed by a train of
+    refocusing pulses, each producing a spin echo [Meiboom]_:
+
+    .. code-block:: text
+
+     Excitation    α₁                    α₂
+        | --TE/2-- | --TE/2-- | --TE/2-- | --TE/2-- | ...
+                 Echo 1                Echo 2
+
+    When all refocusing flip angles are 180° and
+    ``relative_b1 = 1``, the echo amplitudes follow a pure T2 decay.
+    If the effective flip angles deviate from 180° (reduced flip angles
+    or B1 inhomogeneity), stimulated-echo pathways arise and the signal becomes
+    dependent on both T1 and T2.
+
+    References
+    ----------
+    .. [Meiboom] Meiboom S, Gill D. Modified spin-echo method for measuring nuclear relaxation times.
+           Rev Sci Instrum. 1958
+    """
+
+    def __init__(
+        self, flip_angles: torch.Tensor, rf_phases: torch.Tensor, echo_time: float = 0.02, n_states: int = 32
+    ) -> None:
+        """Initialize the multi-echo spin echo signal model.
+
+        Parameters
+        ----------
+        flip_angles
+            Flip angles of the refocusing pulses in rad.
+            Shape `(n_echoes,)`.
+        rf_phases
+            RF phases of the refocusing pulses in rad.
+            Shape `(n_echoes,)`.
+        echo_time
+            Echo time in seconds.
+        n_states
+            Number of EPG states to simulate.
+            Truncating the number of states speeds up simulation at the cost of accuracy.
+        """
+        super().__init__()
+        tse = TseBlock(
+            refocusing_flip_angles=flip_angles,
+            refocusing_rf_phases=rf_phases,
+            te=echo_time,
+        )
+        self.sequence = EPGSequence((tse,))
+        self.n_states = n_states
+
+    def __call__(
+        self, m0: torch.Tensor, t1: torch.Tensor, t2: torch.Tensor, relative_b1: torch.Tensor | None = None
+    ) -> tuple[torch.Tensor]:
+        """Simulate the multi-echo spin echo signal.
+
+        All input tensors are broadcast together to determine the output shape.
+
+        Parameters
+        ----------
+        m0
+            Equilibrium magnetization / proton density.
+            Shape `(...)`, for example `(*other, coils, z, y, x)` or `(samples)`.
+        t1
+            Longitudinal (T1) relaxation time in seconds.
+            Shape `(...)`, for example `(*other, coils, z, y, x)` or`(samples)`.
+        t2
+            Transversal (T2) relaxation time in seconds.
+            Shape `(...)`, for example `(*other, coils, z, y, x)` or `(samples)`.
+        relative_b1
+            Relative B1 amplitude scaling factor. If None, no B1 inhomogeneity is applied.
+            Shape `(...)`, for example `(*other, coils, z, y, x)` or `(samples)`.
+
+        Returns
+        -------
+            Simulated multi-echo spin echo signal.
+            Shape `(echoes, ...)`, for example `(echoes, *other, coils, z, y, x)`
+            or `(echoes, samples)`, where `echoes` corresponds to the number of refocusing pulses in the
+            echo train.
+        """
+        return super().__call__(m0, t1, t2, relative_b1)
+
+    def forward(
+        self, m0: torch.Tensor, t1: torch.Tensor, t2: torch.Tensor, relative_b1: torch.Tensor | None = None
+    ) -> tuple[torch.Tensor]:
+        """Apply forward of MESE.
+
+        .. note::
+            Prefer calling the instance of the MESE as ``operator(x)`` over
+            directly calling this method. See this PyTorch `discussion <https://discuss.pytorch.org/t/is-model-forward-x-the-same-as-model-call-x/33460/3>`_.
+        """
+        parameters = Parameters(m0, t1, t2, relative_b1)
+        _, signals = self.sequence(parameters, states=self.n_states)
+        shape = torch.broadcast_shapes(m0.shape, t1.shape, t2.shape)
+        if relative_b1 is not None:
+            shape = torch.broadcast_shapes(shape, relative_b1.shape)
+        signal = torch.stack(signals, dim=0).broadcast_to(-1, *shape)
+        return (signal,)

src/mrpro/operators/models/__init__.py
@@ -10,6 +10,8 @@ from mrpro.operators.models.MonoExponentialDecay import MonoExponentialDecay
 from mrpro.operators.models.cMRF import CardiacFingerprinting
 from mrpro.operators.models.TransientSteadyStateWithPreparation import TransientSteadyStateWithPreparation
 from mrpro.operators.models import EPG
+from mrpro.operators.models.MESE import MultiEchoSpinEcho
+from mrpro.operators.models.NeuroMRF import NeuroMRF
 
 __all__ = [
     "CardiacFingerprinting",
@@ -17,6 +19,8 @@ __all__ = [
     "InversionRecovery",
     "MOLLI",
     "MonoExponentialDecay",
+    "MultiEchoSpinEcho",
+    "NeuroMRF",
     "SaturationRecovery",
     "SpoiledGRE",
     "TransientSteadyStateWithPreparation",

tests/operators/models/test_mese.py
@@ -1,0 +1,73 @@
+"""Tests for MultiEchoSpinEcho signal model."""
+
+from collections.abc import Sequence
+
+import pytest
+import torch
+from mrpro.operators.models.MESE import MultiEchoSpinEcho
+from mrpro.utils import RandomGenerator
+from tests import autodiff_test
+
+
+def test_mese_basic(parameter_shape: Sequence[int] = (2, 1, 3, 4, 2)) -> None:
+    """Test the MultiEchoSpinEcho model."""
+    n_echos = 10
+    model = MultiEchoSpinEcho(
+        flip_angles=torch.full((n_echos,), torch.pi),
+        rf_phases=torch.full((n_echos,), 0),
+        echo_time=0.018,
+    )
+    rng = RandomGenerator(0)
+    m0 = rng.complex64_tensor(parameter_shape)
+    t1 = rng.float32_tensor(parameter_shape, low=1e-5, high=5)
+    t2 = rng.float32_tensor(parameter_shape, low=1e-5, high=0.5)
+    relative_b1 = None
+    (signal,) = model(m0, t1, t2, relative_b1)
+    assert signal.isfinite().all()
+    assert signal.shape == (n_echos, *parameter_shape)
+
+
+def test_mese_autodiff(parameter_shape: Sequence[int] = (2, 1, 3, 4, 2)) -> None:
+    """Test autodiff works for MultiEchoSpinEcho model."""
+    rng = RandomGenerator(8)
+    n_echos = 10
+    model = MultiEchoSpinEcho(
+        flip_angles=torch.full((n_echos,), torch.pi),
+        rf_phases=torch.full((n_echos,), 0.0),
+    )
+    t1 = rng.float32_tensor(parameter_shape, low=1e-5, high=5)
+    t2 = rng.float32_tensor(parameter_shape, low=1e-5, high=0.5)
+    m0 = rng.complex64_tensor(parameter_shape)
+    relative_b1 = rng.float32_tensor(parameter_shape, low=0.9, high=1.1)
+    autodiff_test(model, m0, t1, t2, relative_b1)
+
+
+@pytest.mark.cuda
+def test_mese_cuda(parameter_shape: Sequence[int] = (2,)) -> None:
+    """Test the MultiEchoSpinEcho model works on cuda devices."""
+    rng = RandomGenerator(8)
+    n_echos = 10
+    flip_angles = rng.float32_tensor(n_echos, low=1e-5, high=5)
+    rf_phases = rng.float32_tensor(n_echos, low=1e-5, high=0.5)
+    t1 = rng.float32_tensor(parameter_shape, low=1e-5, high=5)
+    t2 = rng.float32_tensor(parameter_shape, low=1e-5, high=0.5)
+    m0 = rng.complex64_tensor(parameter_shape)
+    # Create on CPU, transfer to GPU and run on GPU
+    model = MultiEchoSpinEcho(flip_angles=flip_angles, rf_phases=rf_phases)
+    model.cuda()
+    (signal,) = model(m0.cuda(), t1.cuda(), t2.cuda())
+    assert signal.is_cuda
+    assert signal.isfinite().all()
+
+    # Create on GPU and run on GPU
+    model = MultiEchoSpinEcho(flip_angles=flip_angles.cuda(), rf_phases=rf_phases.cuda())
+    (signal,) = model(m0.cuda(), t1.cuda(), t2.cuda())
+    assert signal.is_cuda
+    assert signal.isfinite().all()
+
+    # Create on GPU, transfer to CPU and run on CPU
+    model = MultiEchoSpinEcho(flip_angles=flip_angles.cuda(), rf_phases=rf_phases.cuda())
+    model.cpu()
+    (signal,) = model(m0, t1, t2)
+    assert signal.is_cpu
+    assert signal.isfinite().all()

tests/operators/models/test_neuro_mrf.py
@@ -1,0 +1,61 @@
+"""Tests for MultiEchoSpinEcho signal model."""
+
+from collections.abc import Sequence
+
+import pytest
+from mrpro.operators.models.NeuroMRF import DELICS_FLIP_ANGLES, NeuroMRF
+from mrpro.utils import RandomGenerator
+from tests import autodiff_test
+
+
+def test_neuro_mrf_basic(parameter_shape: Sequence[int] = (2, 1, 3, 4, 2)) -> None:
+    """Test the NeuroMRF model."""
+    model = NeuroMRF()
+    rng = RandomGenerator(0)
+    m0 = rng.complex64_tensor(parameter_shape)
+    t1 = rng.float32_tensor(parameter_shape, low=1e-5, high=5)
+    t2 = rng.float32_tensor(parameter_shape, low=1e-5, high=0.5)
+    relative_b1 = None
+    (signal,) = model(m0, t1, t2, relative_b1)
+    assert signal.isfinite().all()
+    assert signal.shape == (len(DELICS_FLIP_ANGLES), *parameter_shape)
+
+
+def test_neuro_mrf_autodiff(parameter_shape: Sequence[int] = (2, 1, 3, 4, 2)) -> None:
+    """Test autodiff works for NeuroMRF model."""
+    rng = RandomGenerator(8)
+    model = NeuroMRF()
+    t1 = rng.float32_tensor(parameter_shape, low=1e-5, high=5)
+    t2 = rng.float32_tensor(parameter_shape, low=1e-5, high=0.5)
+    m0 = rng.complex64_tensor(parameter_shape)
+    relative_b1 = rng.float32_tensor(parameter_shape, low=0.9, high=1.1)
+    autodiff_test(model, m0, t1, t2, relative_b1)
+
+
+@pytest.mark.cuda
+def test_mese_cuda(parameter_shape: Sequence[int] = (2,)) -> None:
+    """Test the NeuroMRF model works on cuda devices."""
+    rng = RandomGenerator(8)
+
+    t1 = rng.float32_tensor(parameter_shape, low=1e-5, high=5)
+    t2 = rng.float32_tensor(parameter_shape, low=1e-5, high=0.5)
+    m0 = rng.complex64_tensor(parameter_shape)
+    # Create on CPU, transfer to GPU and run on GPU
+    model = NeuroMRF()
+    model.cuda()
+    (signal,) = model(m0.cuda(), t1.cuda(), t2.cuda())
+    assert signal.is_cuda
+    assert signal.isfinite().all()
+
+    # Create on GPU and run on GPU
+    model = NeuroMRF(DELICS_FLIP_ANGLES.cuda())
+    (signal,) = model(m0.cuda(), t1.cuda(), t2.cuda())
+    assert signal.is_cuda
+    assert signal.isfinite().all()
+
+    # Create on GPU, transfer to CPU and run on CPU
+    model = NeuroMRF(DELICS_FLIP_ANGLES.cuda())
+    model.cpu()
+    (signal,) = model(m0, t1, t2)
+    assert signal.is_cpu
+    assert signal.isfinite().all()

fzimmermann89/mr2

(manual backport based on commit 4b755262fa9c55288c6173ab8b4eb679227ea9a5)

src/mr2/operators/models/MESE.py
@@ -1,0 +1,110 @@
+"""Multi-echo spin echo (MESE) signal model (EPG)."""
+
+import torch
+
+from mr2.operators.models.EPG import EPGSequence, Parameters, TseBlock
+from mr2.operators.SignalModel import SignalModel
+
+
+class MultiEchoSpinEcho(SignalModel[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor | None]):
+    """Multi-echo spin echo (MESE) signal model.
+
+    This model simulates a multi-echo spin echo sequence using the extended
+    phase graph (`~mr2.operators.models.EPG`) formalism.
+
+    The sequence consists of an excitation pulse followed by a train of
+    refocusing pulses, each producing a spin echo [Meiboom]_:
+
+    .. code-block:: text
+
+     Excitation    α₁                    α₂
+        | --TE/2-- | --TE/2-- | --TE/2-- | --TE/2-- | ...
+                 Echo 1                Echo 2
+
+    When all refocusing flip angles are 180° and
+    ``relative_b1 = 1``, the echo amplitudes follow a pure T2 decay.
+    If the effective flip angles deviate from 180° (reduced flip angles
+    or B1 inhomogeneity), stimulated-echo pathways arise and the signal becomes
+    dependent on both T1 and T2.
+
+    References
+    ----------
+    .. [Meiboom] Meiboom S, Gill D. Modified spin-echo method for measuring nuclear relaxation times.
+           Rev Sci Instrum. 1958
+    """
+
+    def __init__(
+        self, flip_angles: torch.Tensor, rf_phases: torch.Tensor, echo_time: float = 0.02, n_states: int = 32
+    ) -> None:
+        """Initialize the multi-echo spin echo signal model.
+
+        Parameters
+        ----------
+        flip_angles
+            Flip angles of the refocusing pulses in rad.
+            Shape `(n_echoes,)`.
+        rf_phases
+            RF phases of the refocusing pulses in rad.
+            Shape `(n_echoes,)`.
+        echo_time
+            Echo time in seconds.
+        n_states
+            Number of EPG states to simulate.
+            Truncating the number of states speeds up simulation at the cost of accuracy.
+        """
+        super().__init__()
+        tse = TseBlock(
+            refocusing_flip_angles=flip_angles,
+            refocusing_rf_phases=rf_phases,
+            te=echo_time,
+        )
+        self.sequence = EPGSequence((tse,))
+        self.n_states = n_states
+
+    def __call__(
+        self, m0: torch.Tensor, t1: torch.Tensor, t2: torch.Tensor, relative_b1: torch.Tensor | None = None
+    ) -> tuple[torch.Tensor]:
+        """Simulate the multi-echo spin echo signal.
+
+        All input tensors are broadcast together to determine the output shape.
+
+        Parameters
+        ----------
+        m0
+            Equilibrium magnetization / proton density.
+            Shape `(...)`, for example `(*other, coils, z, y, x)` or `(samples)`.
+        t1
+            Longitudinal (T1) relaxation time in seconds.
+            Shape `(...)`, for example `(*other, coils, z, y, x)` or`(samples)`.
+        t2
+            Transversal (T2) relaxation time in seconds.
+            Shape `(...)`, for example `(*other, coils, z, y, x)` or `(samples)`.
+        relative_b1
+            Relative B1 amplitude scaling factor. If None, no B1 inhomogeneity is applied.
+            Shape `(...)`, for example `(*other, coils, z, y, x)` or `(samples)`.
+
+        Returns
+        -------
+            Simulated multi-echo spin echo signal.
+            Shape `(echoes, ...)`, for example `(echoes, *other, coils, z, y, x)`
+            or `(echoes, samples)`, where `echoes` corresponds to the number of refocusing pulses in the
+            echo train.
+        """
+        return super().__call__(m0, t1, t2, relative_b1)
+
+    def forward(
+        self, m0: torch.Tensor, t1: torch.Tensor, t2: torch.Tensor, relative_b1: torch.Tensor | None = None
+    ) -> tuple[torch.Tensor]:
+        """Apply forward of MESE.
+
+        .. note::
+            Prefer calling the instance of the MESE as ``operator(x)`` over
+            directly calling this method. See this PyTorch `discussion <https://discuss.pytorch.org/t/is-model-forward-x-the-same-as-model-call-x/33460/3>`_.
+        """
+        parameters = Parameters(m0, t1, t2, relative_b1)
+        _, signals = self.sequence(parameters, states=self.n_states)
+        shape = torch.broadcast_shapes(m0.shape, t1.shape, t2.shape)
+        if relative_b1 is not None:
+            shape = torch.broadcast_shapes(shape, relative_b1.shape)
+        signal = torch.stack(signals, dim=0).broadcast_to(-1, *shape)
+        return (signal,)

src/mr2/operators/models/__init__.py
@@ -7,6 +7,8 @@ from mr2.operators.models.MOLLI import MOLLI
 from mr2.operators.models.WASABI import WASABI
 from mr2.operators.models.WASABITI import WASABITI
 from mr2.operators.models.MonoExponentialDecay import MonoExponentialDecay
+from mr2.operators.models.MESE import MultiEchoSpinEcho
+from mr2.operators.models.NeuroMRF import NeuroMRF
 from mr2.operators.models.cMRF import CardiacFingerprinting
 from mr2.operators.models.TransientSteadyStateWithPreparation import TransientSteadyStateWithPreparation
 from mr2.operators.models import EPG
@@ -17,6 +19,8 @@ __all__ = [
     "InversionRecovery",
     "MOLLI",
     "MonoExponentialDecay",
+    "MultiEchoSpinEcho",
+    "NeuroMRF",
     "SaturationRecovery",
     "SpoiledGRE",
     "TransientSteadyStateWithPreparation",

tests/operators/models/test_mese.py
@@ -1,0 +1,73 @@
+"""Tests for MultiEchoSpinEcho signal model."""
+
+from collections.abc import Sequence
+
+import pytest
+import torch
+from mr2.operators.models.MESE import MultiEchoSpinEcho
+from mr2.utils import RandomGenerator
+from tests import autodiff_test
+
+
+def test_mese_basic(parameter_shape: Sequence[int] = (2, 1, 3, 4, 2)) -> None:
+    """Test the MultiEchoSpinEcho model."""
+    n_echos = 10
+    model = MultiEchoSpinEcho(
+        flip_angles=torch.full((n_echos,), torch.pi),
+        rf_phases=torch.full((n_echos,), 0),
+        echo_time=0.018,
+    )
+    rng = RandomGenerator(0)
+    m0 = rng.complex64_tensor(parameter_shape)
+    t1 = rng.float32_tensor(parameter_shape, low=1e-5, high=5)
+    t2 = rng.float32_tensor(parameter_shape, low=1e-5, high=0.5)
+    relative_b1 = None
+    (signal,) = model(m0, t1, t2, relative_b1)
+    assert signal.isfinite().all()
+    assert signal.shape == (n_echos, *parameter_shape)
+
+
+def test_mese_autodiff(parameter_shape: Sequence[int] = (2, 1, 3, 4, 2)) -> None:
+    """Test autodiff works for MultiEchoSpinEcho model."""
+    rng = RandomGenerator(8)
+    n_echos = 10
+    model = MultiEchoSpinEcho(
+        flip_angles=torch.full((n_echos,), torch.pi),
+        rf_phases=torch.full((n_echos,), 0.0),
+    )
+    t1 = rng.float32_tensor(parameter_shape, low=1e-5, high=5)
+    t2 = rng.float32_tensor(parameter_shape, low=1e-5, high=0.5)
+    m0 = rng.complex64_tensor(parameter_shape)
+    relative_b1 = rng.float32_tensor(parameter_shape, low=0.9, high=1.1)
+    autodiff_test(model, m0, t1, t2, relative_b1)
+
+
+@pytest.mark.cuda
+def test_mese_cuda(parameter_shape: Sequence[int] = (2,)) -> None:
+    """Test the MultiEchoSpinEcho model works on cuda devices."""
+    rng = RandomGenerator(8)
+    n_echos = 10
+    flip_angles = rng.float32_tensor(n_echos, low=1e-5, high=5)
+    rf_phases = rng.float32_tensor(n_echos, low=1e-5, high=0.5)
+    t1 = rng.float32_tensor(parameter_shape, low=1e-5, high=5)
+    t2 = rng.float32_tensor(parameter_shape, low=1e-5, high=0.5)
+    m0 = rng.complex64_tensor(parameter_shape)
+    # Create on CPU, transfer to GPU and run on GPU
+    model = MultiEchoSpinEcho(flip_angles=flip_angles, rf_phases=rf_phases)
+    model.cuda()
+    (signal,) = model(m0.cuda(), t1.cuda(), t2.cuda())
+    assert signal.is_cuda
+    assert signal.isfinite().all()
+
+    # Create on GPU and run on GPU
+    model = MultiEchoSpinEcho(flip_angles=flip_angles.cuda(), rf_phases=rf_phases.cuda())
+    (signal,) = model(m0.cuda(), t1.cuda(), t2.cuda())
+    assert signal.is_cuda
+    assert signal.isfinite().all()
+
+    # Create on GPU, transfer to CPU and run on CPU
+    model = MultiEchoSpinEcho(flip_angles=flip_angles.cuda(), rf_phases=rf_phases.cuda())
+    model.cpu()
+    (signal,) = model(m0, t1, t2)
+    assert signal.is_cpu
+    assert signal.isfinite().all()

tests/operators/models/test_neuro_mrf.py
@@ -1,0 +1,61 @@
+"""Tests for MultiEchoSpinEcho signal model."""
+
+from collections.abc import Sequence
+
+import pytest
+from mr2.operators.models.NeuroMRF import DELICS_FLIP_ANGLES, NeuroMRF
+from mr2.utils import RandomGenerator
+from tests import autodiff_test
+
+
+def test_neuro_mrf_basic(parameter_shape: Sequence[int] = (2, 1, 3, 4, 2)) -> None:
+    """Test the NeuroMRF model."""
+    model = NeuroMRF()
+    rng = RandomGenerator(0)
+    m0 = rng.complex64_tensor(parameter_shape)
+    t1 = rng.float32_tensor(parameter_shape, low=1e-5, high=5)
+    t2 = rng.float32_tensor(parameter_shape, low=1e-5, high=0.5)
+    relative_b1 = None
+    (signal,) = model(m0, t1, t2, relative_b1)
+    assert signal.isfinite().all()
+    assert signal.shape == (len(DELICS_FLIP_ANGLES), *parameter_shape)
+
+
+def test_neuro_mrf_autodiff(parameter_shape: Sequence[int] = (2, 1, 3, 4, 2)) -> None:
+    """Test autodiff works for NeuroMRF model."""
+    rng = RandomGenerator(8)
+    model = NeuroMRF()
+    t1 = rng.float32_tensor(parameter_shape, low=1e-5, high=5)
+    t2 = rng.float32_tensor(parameter_shape, low=1e-5, high=0.5)
+    m0 = rng.complex64_tensor(parameter_shape)
+    relative_b1 = rng.float32_tensor(parameter_shape, low=0.9, high=1.1)
+    autodiff_test(model, m0, t1, t2, relative_b1)
+
+
+@pytest.mark.cuda
+def test_mese_cuda(parameter_shape: Sequence[int] = (2,)) -> None:
+    """Test the NeuroMRF model works on cuda devices."""
+    rng = RandomGenerator(8)
+
+    t1 = rng.float32_tensor(parameter_shape, low=1e-5, high=5)
+    t2 = rng.float32_tensor(parameter_shape, low=1e-5, high=0.5)
+    m0 = rng.complex64_tensor(parameter_shape)
+    # Create on CPU, transfer to GPU and run on GPU
+    model = NeuroMRF()
+    model.cuda()
+    (signal,) = model(m0.cuda(), t1.cuda(), t2.cuda())
+    assert signal.is_cuda
+    assert signal.isfinite().all()
+
+    # Create on GPU and run on GPU
+    model = NeuroMRF(DELICS_FLIP_ANGLES.cuda())
+    (signal,) = model(m0.cuda(), t1.cuda(), t2.cuda())
+    assert signal.is_cuda
+    assert signal.isfinite().all()
+
+    # Create on GPU, transfer to CPU and run on CPU
+    model = NeuroMRF(DELICS_FLIP_ANGLES.cuda())
+    model.cpu()
+    (signal,) = model(m0, t1, t2)
+    assert signal.is_cpu
+    assert signal.isfinite().all()