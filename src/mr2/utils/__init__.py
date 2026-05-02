PTB-MR/mrpro

Example for MCIR (#790)
Co-authored-by: Gitinc <noah.jaitner@charite.de>
Co-authored-by: Felix Zimmermann <fzimmermann89@gmail.com>
Co-authored-by: koflera <andreas_kofler@gmx.net>
Co-authored-by: Andreas Kofler <andreas.kofler@ptb.de>
Co-authored-by: JS <30378104+schattenfroh@users.noreply.github.com>

src/mrpro/utils/__init__.py
@@ -13,7 +13,7 @@ from mrpro.utils.sliding_window import sliding_window
 from mrpro.utils.summarize import summarize_object, summarize_values
 from mrpro.utils.reshape import broadcast_right, broadcasted_rearrange, unsqueeze_left, unsqueeze_right, reduce_view, reshape_broadcasted, ravel_multi_index, unsqueeze_tensors_left, unsqueeze_tensors_right, unsqueeze_at, unsqueeze_tensors_at, broadcasted_concatenate, normalize_index, normalize_indices
 from mrpro.utils.TensorAttributeMixin import TensorAttributeMixin
-from mrpro.utils.interpolate import interpolate, apply_lowres
+from mrpro.utils.interpolate import interp, interpolate, apply_lowres
 from mrpro.utils.RandomGenerator import RandomGenerator
 
 __all__ = [
@@ -25,6 +25,7 @@ __all__ = [
     "broadcasted_concatenate",
     "broadcasted_rearrange",
     "fill_range_",
+    "interp",
     "interpolate",
     "normalize_index",
     "normalize_indices",

fzimmermann89/mr2

(manual backport based on commit 6387008c5d9b2da996195d1aaba69ec1fbf94c59)

src/mr2/utils/__init__.py
@@ -13,7 +13,7 @@ from mr2.utils.sliding_window import sliding_window
 from mr2.utils.summarize import summarize_object, summarize_values
 from mr2.utils.reshape import broadcast_right, broadcasted_rearrange, unsqueeze_left, unsqueeze_right, reduce_view, reshape_broadcasted, ravel_multi_index, unsqueeze_tensors_left, unsqueeze_tensors_right, unsqueeze_at, unsqueeze_tensors_at, broadcasted_concatenate, normalize_index, normalize_indices
 from mr2.utils.TensorAttributeMixin import TensorAttributeMixin
-from mr2.utils.interpolate import interpolate, apply_lowres
+from mr2.utils.interpolate import interp, interpolate, apply_lowres
 from mr2.utils.RandomGenerator import RandomGenerator
 from mr2.utils.to_tuple import to_tuple
 from mr2.utils.ema import EMADict
@@ -28,6 +28,7 @@ __all__ = [
     "broadcasted_concatenate",
     "broadcasted_rearrange",
     "fill_range_",
+    "interp",
     "interpolate",
     "normalize_index",
     "normalize_indices",